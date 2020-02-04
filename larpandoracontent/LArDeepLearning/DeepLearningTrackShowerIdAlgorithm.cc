736162a61dd8f86fe7808cd6a0adca5a7c9b6cd2
Andrew Chappell <A.Chappell@warwick.ac.uk>
2020-02-04 10:39:09 +0000

    Add sparsification to inference

--- larpandoracontent/LArDeepLearning/DeepLearningTrackShowerIdAlgorithm.cc
+++ larpandoracontent/LArDeepLearning/DeepLearningTrackShowerIdAlgorithm.cc
@@ -199,19 +199,6 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Train()
 
 StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
 {
-    std::cout << "system_clock " << std::chrono::system_clock::period::num << " " <<
-        std::chrono::system_clock::period::den << " steady: " << std::boolalpha <<
-        std::chrono::system_clock::is_steady << std::endl;
-
-    std::cout << "high_resolution_clock " << std::chrono::high_resolution_clock::period::num << " " <<
-        std::chrono::high_resolution_clock::period::den << " steady: " << std::boolalpha <<
-        std::chrono::high_resolution_clock::is_steady << std::endl;
-
-    std::cout << "steady_clock " << std::chrono::steady_clock::period::num << " " <<
-        std::chrono::steady_clock::period::den << " steady: " << std::boolalpha <<
-        std::chrono::steady_clock::is_steady << std::endl;
-  
-    auto start = std::chrono::steady_clock::now();
     std::shared_ptr<torch::jit::script::Module> pModule(nullptr);
     try
     {
@@ -222,16 +209,12 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
         std::cout << "Error loading the PyTorch module" << std::endl;
         return STATUS_CODE_FAILURE;
     }
-    auto end = std::chrono::steady_clock::now();
-    auto diff = end - start;
-    std::cout << "Network setup time: " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
 
     if (m_visualize)
         PANDORA_MONITORING_API(SetEveDisplayParameters(this->GetPandora(), true, DETECTOR_VIEW_XZ, -1.f, 1.f, 1.f));
 
     for (const std::string listName : m_caloHitListNames)
     {
-        start = std::chrono::steady_clock::now();
         const CaloHitList *pCaloHitList(nullptr);
         PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, listName, pCaloHitList));
 
@@ -265,21 +248,8 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
         if (std::fmod(xRange, tileSize) == 0.f) ++nTilesX;
         if (std::fmod(zRange, tileSize) == 0.f) ++nTilesZ;
 
-        const int nTiles = nTilesX * nTilesZ;
-
-        // Normalisation value from training set - should store and load this from file
-        const float PIXEL_VALUE = (255.0 - 0.558497428894043) / 11.920382499694824;
-        // Populate tensor - may need to split the batch into separate single image batches
-        torch::Tensor input = torch::zeros({nTiles, 1, imageHeight, imageWidth});
-        typedef std::map<const CaloHit*, std::tuple<int, int, int, int>> CaloHitToPixelMap;
-        CaloHitToPixelMap caloHitToPixelMap;
-        auto accessor = input.accessor<float, 4>();
-        end = std::chrono::steady_clock::now();
-        diff = end - start;
-        std::cout << "#Hits: " << pCaloHitList->size() << " #TilesX: " <<
-            nTilesX << " #TilesZ: " << nTilesZ << " #Tiles: " << nTiles << std::endl;
-        std::cout << "Prep time: " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
-        start = std::chrono::steady_clock::now();
+        // Identify the tiles that actually contain hits
+        std::map<int, bool> tilePopulationMap;
         for (const CaloHit *pCaloHit : *pCaloHitList)
         {
             const float x(pCaloHit->GetPositionVector().GetX());
@@ -288,6 +258,37 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
             const int tileX = static_cast<int>(std::floor((x - xMin) / tileSize));
             const int tileZ = static_cast<int>(std::floor((z - zMin) / tileSize));
             const int tile = tileZ * nTilesX + tileX;
+            tilePopulationMap.insert(std::make_pair(tile, true));
+        }
+
+        int nextTile = 0;
+        std::map<int, int> sparseMap;
+        for(auto element : tilePopulationMap)
+        {
+            if (element.second)
+            {
+                sparseMap.insert(std::make_pair(element.first, nextTile));
+                ++nextTile;
+            }
+        }
+
+        const int nTiles = nextTile;//nTilesX * nTilesZ;
+
+        // Normalisation value from training set - should store and load this from file
+        const float PIXEL_VALUE = (255.0 - 0.558497428894043) / 11.920382499694824;
+        // Populate tensor - may need to split the batch into separate single image batches
+        torch::Tensor input = torch::zeros({nTiles, 1, imageHeight, imageWidth});
+        typedef std::map<const CaloHit*, std::tuple<int, int, int, int>> CaloHitToPixelMap;
+        CaloHitToPixelMap caloHitToPixelMap;
+        auto accessor = input.accessor<float, 4>();
+        for (const CaloHit *pCaloHit : *pCaloHitList)
+        {
+            const float x(pCaloHit->GetPositionVector().GetX());
+            const float z(pCaloHit->GetPositionVector().GetZ());
+            // Determine which tile the hit will be assigned to
+            const int tileX = static_cast<int>(std::floor((x - xMin) / tileSize));
+            const int tileZ = static_cast<int>(std::floor((z - zMin) / tileSize));
+            const int tile = sparseMap.at(tileZ * nTilesX + tileX);
             // Determine hit position within the tile
             const float localX = std::fmod(x - xMin, tileSize);
             const float localZ = std::fmod(z - zMin, tileSize);
@@ -301,31 +302,21 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
         // Pass as input the input Tensor containing the calo hit picture
         std::vector<torch::jit::IValue> inputs;
         inputs.push_back(input);
-        end = std::chrono::steady_clock::now();
-        diff = end - start;
-        std::cout << "Image processing time: " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
 
         // Run the input through the trained model and get the output accessor
-        // There should be one iteration of this for each image associated with a given tile
-        // Need to ensure that each calohit is mapped to the correct tile and extract the
-        // track/shower probability from the appropriate pixel within that particular ouput
-        // This may need multiple runs of forward on the different tile inputs - essentially
-        // this would need to know about the value in the first dimension of the outputAccessor -
-        // i.e. the number within the batch
-        start = std::chrono::steady_clock::now();
+        auto start = std::chrono::steady_clock::now();
         at::Tensor output = pModule->forward(inputs).toTensor();
         auto outputAccessor = output.accessor<float, 4>();
-        end = std::chrono::steady_clock::now();
-        diff = end - start;
+        auto end = std::chrono::steady_clock::now();
+        auto diff = end - start;
         std::cout << "Network time: " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
 
-        start = std::chrono::steady_clock::now();
         for (const CaloHit *pCaloHit : *pCaloHitList)
         {
             auto pixelMap = caloHitToPixelMap.at(pCaloHit);
             const int tileX(std::get<0>(pixelMap));
             const int tileZ(std::get<1>(pixelMap));
-            const int tile = tileZ * nTilesX + tileX;
+            const int tile = sparseMap.at(tileZ * nTilesX + tileX);
             const int pixelX(std::get<2>(pixelMap));
             const int pixelZ(std::get<3>(pixelMap));
 
@@ -336,97 +327,23 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
             if (statusCode != STATUS_CODE_SUCCESS)
                 std::cout << "Cannot set calo hit meta data" << std::endl;
         }
-        end = std::chrono::steady_clock::now();
-        diff = end - start;
-        std::cout << "Readout time " << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
 
         /*
-
-        const float zMin(isU ? m_zMinU : (isV ? m_zMinV : m_zMinW));
-        const float zMax(isU ? m_zMaxU : (isV ? m_zMaxV : m_zMaxW));
-
-        const float xSpan(m_xMax - m_xMin), zSpan(zMax - zMin);
-
-        typedef std::map<const CaloHit*, std::pair<int, int>> CaloHitToBinMap;
-        CaloHitToBinMap caloHitToBinMap;
-
-        // Start with RGB picture of black pixels.  Four indices: first default size 1, second index is RGB indices, third is xBin,
-        // fourth is zBin
-        torch::Tensor input = torch::zeros({1, 3, m_nBins, m_nBins});
-        auto accessor = input.accessor<float, 4>();
-
-        // Create a map of calo hits to x/z bin values.  Set the output track shower id of the pixel using the RGB values at the pixel
-        // containing the calo hit
-        for (const CaloHit *pCaloHit : *pCaloHitList)
-        {
-            const float x(pCaloHit->GetPositionVector().GetX());
-            const float z(pCaloHit->GetPositionVector().GetZ());
-
-            const int xBin(std::floor((x-m_xMin)*m_nBins/xSpan));
-            const int zBin(std::floor((z-zMin)*m_nBins/zSpan));
-
-            // ATTN: Set pixels containing a calo hit to white
-            if (xBin >= 0 && xBin <= m_nBins && zBin >= 0 && zBin <= m_nBins)
-            {
-                caloHitToBinMap.insert(std::make_pair(pCaloHit, std::make_pair(xBin, zBin)));
-                accessor[0][0][xBin][zBin] = 1;
-                accessor[0][1][xBin][zBin] = 1;
-                accessor[0][2][xBin][zBin] = 1;
-            }
-        }
-
-        // Pass as input the input Tensor containing the calo hit picture
-        std::vector<torch::jit::IValue> inputs;
-        inputs.push_back(input);
-
-        // Run the input through the trained model and get the output accessor
-        at::Tensor output = pModule->forward(inputs).toTensor();
-        auto outputAccessor = output.accessor<float, 4>();
-
-        // Colour in the shower and track bits (and other) in a visual display for first performance inspection
-        CaloHitList showerHits, trackHits, other;
-
-        for (const CaloHit *pCaloHit : *pCaloHitList)
-        {
-            if (caloHitToBinMap.find(pCaloHit) == caloHitToBinMap.end())
-            {
-                other.push_back(pCaloHit);
-                continue;
-            }
-
-            const int xBin(caloHitToBinMap.at(pCaloHit).first);
-            const int zBin(caloHitToBinMap.at(pCaloHit).second);
-
-            // Is the R value bigger than the B value.  In training the target picture was coloured such that showers were red and tracks blue
-            const bool isShower(outputAccessor[0][0][xBin][zBin] > outputAccessor[0][2][xBin][zBin] ? true : false);
-            object_creation::CaloHit::Metadata metadata;
-
             if (isShower)
-            {
-                metadata.m_propertiesToAdd["IsShower"] = 1.f;
                 showerHits.push_back(pCaloHit);
-            }
             else
-            {
-                metadata.m_propertiesToAdd["IsTrack"] = 1.f;
                 trackHits.push_back(pCaloHit);
+
+            if (m_visualize)
+            {
+                const std::string trackListName("TrackHits_" + listName);
+                const std::string showerListName("ShowerHits_" + listName);
+                const std::string otherListName("OtherHits_" + listName);
+                PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &trackHits, trackListName, BLUE));
+                PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &showerHits, showerListName, RED));
+                PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &other, otherListName, BLACK));
             }
-
-            const StatusCode &statusCode(PandoraContentApi::CaloHit::AlterMetadata(*this, pCaloHit, metadata));
-
-            if (statusCode != STATUS_CODE_SUCCESS)
-                std::cout << "Cannot set calo hit meta data" << std::endl;
-        }
-
-        if (m_visualize)
-        {
-            const std::string trackListName("TrackHits_" + listName);
-            const std::string showerListName("ShowerHits_" + listName);
-            const std::string otherListName("OtherHits_" + listName);
-            PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &trackHits, trackListName, BLUE));
-            PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &showerHits, showerListName, RED));
-            PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &other, otherListName, BLACK));
-        }*/
+         */
     }
 
     if (m_visualize)
