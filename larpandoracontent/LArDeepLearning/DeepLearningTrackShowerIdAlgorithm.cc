c63836753239d04a01dc9acc76c0b5be7954ab0d
Andrew Chappell <A.Chappell@warwick.ac.uk>
2020-02-10 11:08:50 +0000

    Process one tile at-a-time to avoid memory problems in very large events

--- larpandoracontent/LArDeepLearning/DeepLearningTrackShowerIdAlgorithm.cc
+++ larpandoracontent/LArDeepLearning/DeepLearningTrackShowerIdAlgorithm.cc
@@ -218,9 +218,50 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Train()
     return STATUS_CODE_SUCCESS;
 }
 
+void GetHitRegion(const CaloHitList& caloHitList, float& xMin, float& xMax, float& zMin, float& zMax)
+{
+    xMin = std::numeric_limits<float>::max(); xMax = 0.f;
+    zMin = std::numeric_limits<float>::max(); zMax = 0.f;
+    for (const CaloHit *pCaloHit : caloHitList)
+    {
+        const float x(pCaloHit->GetPositionVector().GetX());
+        const float z(pCaloHit->GetPositionVector().GetZ());
+        if (x < xMin) xMin = x;
+        if (x > xMax) xMax = x;
+        if (z < zMin) zMin = z;
+        if (z > zMax) zMax = z;
+    }
+}
+
+void GetSparseTileMap(const CaloHitList& caloHitList, const float xMin,
+        const float zMin, const float tileSize, const int nTilesX, std::map<int, int>& sparseMap)
+{
+    // Identify the tiles that actually contain hits
+    std::map<int, bool> tilePopulationMap;
+    for (const CaloHit *pCaloHit : caloHitList)
+    {
+        const float x(pCaloHit->GetPositionVector().GetX());
+        const float z(pCaloHit->GetPositionVector().GetZ());
+        // Determine which tile the hit will be assigned to
+        const int tileX = static_cast<int>(std::floor((x - xMin) / tileSize));
+        const int tileZ = static_cast<int>(std::floor((z - zMin) / tileSize));
+        const int tile = tileZ * nTilesX + tileX;
+        tilePopulationMap.insert(std::make_pair(tile, true));
+    }
+
+    int nextTile = 0;
+    for(auto element : tilePopulationMap)
+    {
+        if (element.second)
+        {
+            sparseMap.insert(std::make_pair(element.first, nextTile));
+            ++nextTile;
+        }
+    }
+}
+
 StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
 {
-    //torch::Tensor tensor = torch::rand({2, 3});
     auto start = std::chrono::steady_clock::now();
     torch::jit::script::Module pModule;
     try
@@ -252,17 +293,8 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
         const int imageWidth = 256;
         const int imageHeight = 256;
         // Get bounds of hit region
-        float xMin = std::numeric_limits<float>::max(); float xMax = 0.f;
-        float zMin = std::numeric_limits<float>::max(); float zMax = 0.f;
-        for (const CaloHit *pCaloHit : *pCaloHitList)
-        {
-            const float x(pCaloHit->GetPositionVector().GetX());
-            const float z(pCaloHit->GetPositionVector().GetZ());
-            if (x < xMin) xMin = x;
-            if (x > xMax) xMax = x;
-            if (z < zMin) zMin = z;
-            if (z > zMax) zMax = z;
-        }
+        float xMin{}; float xMax{}; float zMin{}; float zMax{};
+        GetHitRegion(*pCaloHitList, xMin, xMax, zMin, zMax);
         const float xRange = xMax - xMin;
         const float zRange = zMax - zMin;
         int nTilesX = static_cast<int>(std::ceil(xRange / tileSize));
@@ -271,96 +303,90 @@ StatusCode DeepLearningTrackShowerIdAlgorithm::Infer()
         if (std::fmod(xRange, tileSize) == 0.f) ++nTilesX;
         if (std::fmod(zRange, tileSize) == 0.f) ++nTilesZ;
 
-        // Identify the tiles that actually contain hits
-        std::map<int, bool> tilePopulationMap;
-        for (const CaloHit *pCaloHit : *pCaloHitList)
-        {
-            const float x(pCaloHit->GetPositionVector().GetX());
-            const float z(pCaloHit->GetPositionVector().GetZ());
-            // Determine which tile the hit will be assigned to
-            const int tileX = static_cast<int>(std::floor((x - xMin) / tileSize));
-            const int tileZ = static_cast<int>(std::floor((z - zMin) / tileSize));
-            const int tile = tileZ * nTilesX + tileX;
-            tilePopulationMap.insert(std::make_pair(tile, true));
-        }
-
-        int nextTile = 0;
         std::map<int, int> sparseMap;
-        for(auto element : tilePopulationMap)
-        {
-            if (element.second)
-            {
-                sparseMap.insert(std::make_pair(element.first, nextTile));
-                ++nextTile;
-            }
-        }
-
-        const int nTiles = nextTile;//nTilesX * nTilesZ;
+        GetSparseTileMap(*pCaloHitList, xMin, zMin, tileSize, nTilesX, sparseMap);
+        const int nTiles = sparseMap.size();
 
         // Normalisation value from training set - should store and load this from file
         const float PIXEL_VALUE = (255.0 - 0.558497428894043) / 11.920382499694824;
-        // Populate tensor - may need to split the batch into separate single image batches
-        torch::Tensor input = torch::zeros({nTiles, 1, imageHeight, imageWidth});
-        typedef std::map<const CaloHit*, std::tuple<int, int, int, int>> CaloHitToPixelMap;
-        CaloHitToPixelMap caloHitToPixelMap;
-        auto accessor = input.accessor<float, 4>();
-        for (const CaloHit *pCaloHit : *pCaloHitList)
-        {
-            const float x(pCaloHit->GetPositionVector().GetX());
-            const float z(pCaloHit->GetPositionVector().GetZ());
-            // Determine which tile the hit will be assigned to
-            const int tileX = static_cast<int>(std::floor((x - xMin) / tileSize));
-            const int tileZ = static_cast<int>(std::floor((z - zMin) / tileSize));
-            const int tile = sparseMap.at(tileZ * nTilesX + tileX);
-            // Determine hit position within the tile
-            const float localX = std::fmod(x - xMin, tileSize);
-            const float localZ = std::fmod(z - zMin, tileSize);
-            // Determine hit pixel within the tile
-            const int pixelX = static_cast<int>(std::floor(localX * imageHeight / tileSize));
-            const int pixelZ = static_cast<int>(std::floor(localZ * imageWidth / tileSize));
-            accessor[tile][0][pixelX][pixelZ] = PIXEL_VALUE;
-            caloHitToPixelMap.insert(std::make_pair(pCaloHit, std::make_tuple(tileX, tileZ, pixelX, pixelZ)));
-        }
-
-        // Pass as input the input Tensor containing the calo hit picture
-        std::vector<torch::jit::IValue> inputs;
-        inputs.push_back(input);
-
-        // Run the input through the trained model and get the output accessor
-        at::Tensor output = pModule.forward(inputs).toTensor();
-        auto outputAccessor = output.accessor<float, 4>();
-
         CaloHitList trackHits, showerHits, otherHits;
-        for (const CaloHit *pCaloHit : *pCaloHitList)
+        // Populate tensor - may need to split the batch into separate single image batches
+        //torch::Tensor input = torch::zeros({nTiles, 1, imageHeight, imageWidth});
+        for (int i = 0; i < nTiles; ++i)
         {
-            auto pixelMap = caloHitToPixelMap.at(pCaloHit);
-            const int tileX(std::get<0>(pixelMap));
-            const int tileZ(std::get<1>(pixelMap));
-            const int tile = sparseMap.at(tileZ * nTilesX + tileX);
-            const int pixelX(std::get<2>(pixelMap));
-            const int pixelZ(std::get<3>(pixelMap));
+            torch::Tensor input = torch::zeros({1, 1, imageHeight, imageWidth});
+            typedef std::map<const CaloHit*, std::tuple<int, int, int, int>> CaloHitToPixelMap;
+            CaloHitToPixelMap caloHitToPixelMap;
+            auto accessor = input.accessor<float, 4>();
+            for (const CaloHit *pCaloHit : *pCaloHitList)
+            {
+                const float x(pCaloHit->GetPositionVector().GetX());
+                const float z(pCaloHit->GetPositionVector().GetZ());
+                // Determine which tile the hit will be assigned to
+                const int tileX = static_cast<int>(std::floor((x - xMin) / tileSize));
+                const int tileZ = static_cast<int>(std::floor((z - zMin) / tileSize));
+                const int tile = sparseMap.at(tileZ * nTilesX + tileX);
+                if (tile == i)
+                {
+                    // Determine hit position within the tile
+                    const float localX = std::fmod(x - xMin, tileSize);
+                    const float localZ = std::fmod(z - zMin, tileSize);
+                    // Determine hit pixel within the tile
+                    const int pixelX = static_cast<int>(std::floor(localX * imageHeight / tileSize));
+                    const int pixelZ = static_cast<int>(std::floor(localZ * imageWidth / tileSize));
+                    //accessor[tile][0][pixelX][pixelZ] = PIXEL_VALUE;
+                    accessor[0][0][pixelX][pixelZ] = PIXEL_VALUE;
+                    caloHitToPixelMap.insert(std::make_pair(pCaloHit, std::make_tuple(tileX, tileZ, pixelX, pixelZ)));
+                }
+            }
 
-            object_creation::CaloHit::Metadata metadata;
-            // Apply softmax to loss to get actual probability
-            float probShower = exp(outputAccessor[tile][1][pixelX][pixelZ]);
-            float probTrack = exp(outputAccessor[tile][2][pixelX][pixelZ]);
-            float probNull = exp(outputAccessor[tile][0][pixelX][pixelZ]);
-            float recipSum = 1.f / (probShower + probTrack + probNull);
-            probShower *= recipSum;
-            probTrack *= recipSum;
-            probNull *= recipSum;
-            metadata.m_propertiesToAdd["Pshower"] = probShower;
-            metadata.m_propertiesToAdd["Ptrack"] = probTrack;
-            if (probShower > probTrack && probShower > probNull)
-                showerHits.push_back(pCaloHit);
-            else if (probTrack > probShower && probTrack > probNull)
-                trackHits.push_back(pCaloHit);
-            else
-                otherHits.push_back(pCaloHit);
+            // Pass as input the input Tensor containing the calo hit picture
+            std::vector<torch::jit::IValue> inputs;
+            inputs.push_back(input);
 
-            const StatusCode &statusCode(PandoraContentApi::CaloHit::AlterMetadata(*this, pCaloHit, metadata));
-            if (statusCode != STATUS_CODE_SUCCESS)
-                std::cout << "Cannot set calo hit meta data" << std::endl;
+            // Run the input through the trained model and get the output accessor
+            at::Tensor output = pModule.forward(inputs).toTensor();
+            auto outputAccessor = output.accessor<float, 4>();
+
+            for (const CaloHit *pCaloHit : *pCaloHitList)
+            {
+                auto found = caloHitToPixelMap.find(pCaloHit);
+                if (found == caloHitToPixelMap.end()) continue;
+                auto pixelMap = found->second;
+                //auto pixelMap = caloHitToPixelMap.at(pCaloHit);
+                const int tileX(std::get<0>(pixelMap));
+                const int tileZ(std::get<1>(pixelMap));
+                const int tile = sparseMap.at(tileZ * nTilesX + tileX);
+                if (tile == i)
+                {
+                    const int pixelX(std::get<2>(pixelMap));
+                    const int pixelZ(std::get<3>(pixelMap));
+
+                    object_creation::CaloHit::Metadata metadata;
+                    // Apply softmax to loss to get actual probability
+                    //float probShower = exp(outputAccessor[tile][1][pixelX][pixelZ]);
+                    //float probTrack = exp(outputAccessor[tile][2][pixelX][pixelZ]);
+                    //float probNull = exp(outputAccessor[tile][0][pixelX][pixelZ]);
+                    float probShower = exp(outputAccessor[0][1][pixelX][pixelZ]);
+                    float probTrack = exp(outputAccessor[0][2][pixelX][pixelZ]);
+                    float probNull = exp(outputAccessor[0][0][pixelX][pixelZ]);
+                    float recipSum = 1.f / (probShower + probTrack + probNull);
+                    probShower *= recipSum;
+                    probTrack *= recipSum;
+                    probNull *= recipSum;
+                    metadata.m_propertiesToAdd["Pshower"] = probShower;
+                    metadata.m_propertiesToAdd["Ptrack"] = probTrack;
+                    if (probShower > probTrack && probShower > probNull)
+                        showerHits.push_back(pCaloHit);
+                    else if (probTrack > probShower && probTrack > probNull)
+                        trackHits.push_back(pCaloHit);
+                    else
+                        otherHits.push_back(pCaloHit);
+                    const StatusCode &statusCode(PandoraContentApi::CaloHit::AlterMetadata(*this, pCaloHit, metadata));
+                    if (statusCode != STATUS_CODE_SUCCESS)
+                        std::cout << "Cannot set calo hit meta data" << std::endl;
+                }
+            }
         }
 
         if (m_visualize)
