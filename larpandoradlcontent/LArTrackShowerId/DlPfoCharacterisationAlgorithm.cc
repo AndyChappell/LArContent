0a8e6e5272d2c733d4c4c138a9cee2684d483ef5
Andrew Chappell <A.Chappell@warwick.ac.uk>
2022-07-01 09:15:44 +0100

    Add provisional PFO characterisation network

--- larpandoradlcontent/LArTrackShowerId/DlPfoCharacterisationAlgorithm.cc
+++ larpandoradlcontent/LArTrackShowerId/DlPfoCharacterisationAlgorithm.cc
@@ -8,6 +8,7 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "larpandoracontent/LArHelpers/LArFileHelper.h"
 #include "larpandoracontent/LArHelpers/LArPfoHelper.h"
 #include "larpandoracontent/LArObjects/LArCaloHit.h"
 
@@ -21,96 +22,312 @@ using namespace lar_content;
 namespace lar_dl_content
 {
 
-DlPfoCharacterisationAlgorithm::DlPfoCharacterisationAlgorithm()
+DlPfoCharacterisationAlgorithm::DlPfoCharacterisationAlgorithm() :
+    m_training{false},
+    m_imageWidth{224},
+    m_imageHeight{224}
 {
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-bool DlPfoCharacterisationAlgorithm::IsClearTrack(const Cluster *const pCluster) const
+StatusCode DlPfoCharacterisationAlgorithm::Run()
 {
-    const OrderedCaloHitList &orderedCaloHitList{pCluster->GetOrderedCaloHitList()};
-    CaloHitList caloHits;
-    orderedCaloHitList.FillCaloHitList(caloHits);
-    const CaloHitList &isolatedHits{pCluster->GetIsolatedCaloHitList()};
-    caloHits.insert(caloHits.end(), isolatedHits.begin(), isolatedHits.end());
-    FloatVector trackLikelihoods;
-    try
-    {
-        for (const CaloHit *pCaloHit : caloHits)
-        {
-            const LArCaloHit *pLArCaloHit{dynamic_cast<const LArCaloHit *>(pCaloHit)};
-            const float pTrack{pLArCaloHit->GetTrackProbability()};
-            const float pShower{pLArCaloHit->GetShowerProbability()};
-            if ((pTrack + pShower) > std::numeric_limits<float>::epsilon())
-                trackLikelihoods.emplace_back(pTrack / (pTrack + pShower));
-        }
-
-        const unsigned long N{trackLikelihoods.size()};
-        if (N > 0)
-        {
-            float mean{std::accumulate(std::begin(trackLikelihoods), std::end(trackLikelihoods), 0.f) / N};
-            if (mean >= 0.5f)
-                return true;
-            else
-                return false;
-        }
-    }
-    catch (const StatusCodeException &)
-    {
-    }
-
-    return true;
+    if (m_training)
+        return this->Train();
+    else
+        return this->Infer();
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-bool DlPfoCharacterisationAlgorithm::IsClearTrack(const pandora::ParticleFlowObject *const pPfo) const
+StatusCode DlPfoCharacterisationAlgorithm::Infer()
 {
-    ClusterList allClusters;
-    LArPfoHelper::GetTwoDClusterList(pPfo, allClusters);
-    FloatVector trackLikelihoods;
-    for (const Cluster *pCluster : allClusters)
+    PfoList tracksToShowers, showersToTracks;
+    LArDLHelper::TorchInput input;
+    LArDLHelper::InitialiseInput({1, 3, m_imageHeight, m_imageWidth}, input);
+
+    const PfoList *pPfoList(nullptr);
+
+    for (const bool inputIsTrack : {true, false})
     {
-        const OrderedCaloHitList &orderedCaloHitList{pCluster->GetOrderedCaloHitList()};
-        CaloHitList caloHits;
-        orderedCaloHitList.FillCaloHitList(caloHits);
-        const CaloHitList &isolatedHits{pCluster->GetIsolatedCaloHitList()};
-        caloHits.insert(caloHits.end(), isolatedHits.begin(), isolatedHits.end());
-        try
+        const std::string listName{inputIsTrack ? m_trackPfoListName : m_showerPfoListName};
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, listName, pPfoList));
+
+        for (const ParticleFlowObject *const pPfo : *pPfoList)
         {
-            for (const CaloHit *pCaloHit : caloHits)
+            const HitType viewU{HitType::TPC_VIEW_U}, viewV{HitType::TPC_VIEW_V}, viewW{HitType::TPC_VIEW_W};
+            CaloHitList caloHitList;
+            LArPfoHelper::GetCaloHits(pPfo, viewU, caloHitList);
+            LArPfoHelper::GetCaloHits(pPfo, viewV, caloHitList);
+            LArPfoHelper::GetCaloHits(pPfo, viewW, caloHitList);
+            this->PrepareNetworkInput(caloHitList, input);
+
+            // Run the input through the trained model and get the output accessor
+            LArDLHelper::TorchInputVector inputs;
+            inputs.emplace_back(input);
+            LArDLHelper::TorchOutput output;
+            LArDLHelper::Forward(m_model, inputs, output);
+            auto outputAccessor = output.accessor<float, 2>();
+            // Get class probabilities
+            const double output0{exp(outputAccessor[0][0])};
+            const double output1{exp(outputAccessor[0][1])};
+            const double sum{(output0 + output1) > std::numeric_limits<float>::epsilon() ? output0 + output1 : 1.f};
+            const double prob0{output0 / sum}, prob1{output1 / sum};
+            if (prob0 > prob1)
             {
-                const LArCaloHit *pLArCaloHit{dynamic_cast<const LArCaloHit *>(pCaloHit)};
-                const float pTrack{pLArCaloHit->GetTrackProbability()};
-                const float pShower{pLArCaloHit->GetShowerProbability()};
-                if ((pTrack + pShower) > std::numeric_limits<float>::epsilon())
-                    trackLikelihoods.emplace_back(pTrack / (pTrack + pShower));
+                // Shower-like PFO
+                if (inputIsTrack)
+                {
+                    this->ChangeCharacterisation(pPfo);
+                    tracksToShowers.emplace_back(pPfo);
+                }
+            }
+            else
+            {
+                // Track-like PFO
+                if (!inputIsTrack)
+                {
+                    this->ChangeCharacterisation(pPfo);
+                    showersToTracks.emplace_back(pPfo);
+                }
             }
         }
-        catch (const StatusCodeException &)
+    }
+
+    if (!tracksToShowers.empty())
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::SaveList(*this, m_trackPfoListName, m_showerPfoListName, tracksToShowers));
+
+    if (!showersToTracks.empty())
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::SaveList(*this, m_showerPfoListName, m_trackPfoListName, showersToTracks));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DlPfoCharacterisationAlgorithm::Train()
+{
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->ProcessPfoList(m_trackPfoListName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->ProcessPfoList(m_showerPfoListName));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void DlPfoCharacterisationAlgorithm::PrepareNetworkInput(const CaloHitList &caloHitList, LArDLHelper::TorchInput &input) const
+{
+    const double eps{1.1920929e-7}; // Python float epsilon, used in image padding
+    double xMin{0.}, xMax{0.}, zMin{0.}, zMax{0.};
+
+    this->GetHitRegion(caloHitList, xMin, xMax, zMin, zMax);
+    double xRange{xMax - xMin}, zRange{zMax - zMin};
+
+    // Enforce a minimum region size
+    if (xRange < 112.)
+    {
+        const double pad{0.5 * (112. - xRange)};
+        xMin -= pad;
+        xMax += pad;
+    }
+    if (zRange < 112.)
+    {
+        const double pad{0.5 * (112. - zRange)};
+        zMin -= pad;
+        zMax += pad;
+    }
+    xMin -= eps; xMax += eps;
+    zMin -= eps; zMax += eps;
+    xRange = xMax - xMin;
+    zRange = zMax - zMin;
+
+    float **weights = new float *[m_imageHeight];
+    for (int r = 0; r < m_imageHeight; ++r)
+        weights[r] = new float[m_imageWidth]();
+
+    for (const CaloHit *pCaloHit : caloHitList)
+    {
+        // Determine hit pixel
+        const double x{pCaloHit->GetPositionVector().GetX()}, z{pCaloHit->GetPositionVector().GetZ()};
+        const double localX{(x - xMin) / xRange};
+        const double localZ{(z - zMin) / zRange};
+        const int pixelX{static_cast<int>(std::floor(localX * m_imageWidth))};
+        const int pixelZ{static_cast<int>(std::floor(localZ * m_imageHeight))};
+        weights[pixelZ][pixelX] += pCaloHit->GetInputEnergy();
+    }
+
+    float minWeight{std::numeric_limits<float>::max()}, maxWeight{-std::numeric_limits<float>::max()};
+    for (int r = 0; r < m_imageHeight; ++r)
+    {
+        for (int c = 0; c < m_imageWidth; ++c)
         {
+            if (weights[r][c] < minWeight)
+                minWeight = weights[r][c];
+            if (weights[r][c] > maxWeight)
+                maxWeight = weights[r][c];
         }
     }
+    const float weightRange{maxWeight - minWeight > std::numeric_limits<float>::epsilon() ? maxWeight - minWeight : 1.f};
 
-    const unsigned long N{trackLikelihoods.size()};
-    if (N > 0)
+    auto accessor = input.accessor<float, 4>();
+    // Channel mapping is UVW => RGB (012)
+    const int channel{caloHitList.front()->GetHitType() - HitType::TPC_VIEW_U};
+    for (const CaloHit *pCaloHit : caloHitList)
     {
-        float mean{std::accumulate(std::begin(trackLikelihoods), std::end(trackLikelihoods), 0.f) / N};
-        if (mean >= 0.5f)
-            return true;
-        else
-            return false;
+        const double x(pCaloHit->GetPositionVector().GetX());
+        const double z(pCaloHit->GetPositionVector().GetZ());
+        const double localX{(x - xMin) / xRange};
+        const double localZ{(z - zMin) / zRange};
+        const int pixelX{static_cast<int>(std::floor(localX * m_imageWidth))};
+        const int pixelZ{static_cast<int>(std::floor(localZ * m_imageHeight))};
+        accessor[0][channel][pixelZ][pixelX] = (weights[pixelZ][pixelX] - minWeight) / weightRange;
     }
 
-    return true;
+    for (int r = 0; r < m_imageHeight; ++r)
+        delete [] weights[r];
+    delete[] weights;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void DlPfoCharacterisationAlgorithm::GetHitRegion(const CaloHitList &caloHitList, double &xMin, double &xMax, double &zMin, double &zMax) const
+{
+    xMin = std::numeric_limits<float>::max();
+    xMax = -std::numeric_limits<float>::max();
+    zMin = std::numeric_limits<float>::max();
+    zMax = -std::numeric_limits<float>::max();
+    for (const CaloHit *pCaloHit : caloHitList)
+    {
+        const float x(pCaloHit->GetPositionVector().GetX());
+        const float z(pCaloHit->GetPositionVector().GetZ());
+        if (x < xMin)
+            xMin = x;
+        if (x > xMax)
+            xMax = x;
+        if (z < zMin)
+            zMin = z;
+        if (z > zMax)
+            zMax = z;
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DlPfoCharacterisationAlgorithm::ProcessPfoList(const std::string &pfoListName) const
+{
+    const PfoList *pPfoList(nullptr);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, pfoListName, pPfoList));
+
+    for (const ParticleFlowObject *const pPfo : *pPfoList)
+    {
+        LArMvaHelper::MvaFeatureVector featureVector;
+        float trackContribution{0.f}, showerContribution{0.f};
+        CaloHitList caloHitListU, caloHitListV, caloHitListW;
+        const HitType viewU{HitType::TPC_VIEW_U}, viewV{HitType::TPC_VIEW_V}, viewW{HitType::TPC_VIEW_W};
+        LArPfoHelper::GetCaloHits(pPfo, viewU, caloHitListU);
+        LArPfoHelper::GetCaloHits(pPfo, viewV, caloHitListV);
+        LArPfoHelper::GetCaloHits(pPfo, viewW, caloHitListW);
+        int numGoodViews{0};
+        if (caloHitListU.size() >= 5)
+            ++numGoodViews;
+        if (caloHitListV.size() >= 5)
+            ++numGoodViews;
+        if (caloHitListW.size() >= 5)
+            ++numGoodViews;
+        if (numGoodViews < 2 || (caloHitListU.size() + caloHitListV.size() + caloHitListW.size() < 15))
+            continue;
+        for (const CaloHitList &caloHitList : {caloHitListU, caloHitListV, caloHitListW})
+        {
+            for (const CaloHit *pCaloHit : caloHitList)
+            {
+                try
+                {
+                    const MCParticle *pMCParticle{MCParticleHelper::GetMainMCParticle(pCaloHit)};
+                    const int pdg{std::abs(pMCParticle->GetParticleId())};
+                    if (pdg == E_MINUS || pdg == PHOTON)
+                        showerContribution += pCaloHit->GetInputEnergy();
+                    else
+                        trackContribution += pCaloHit->GetInputEnergy();
+                }
+                catch (const StatusCodeException &)
+                {
+                }
+            }
+        }
+        const float totalContribution{trackContribution + showerContribution};
+        if (totalContribution <= std::numeric_limits<float>::epsilon())
+            continue;
+        const float trackFraction{trackContribution / totalContribution};
+        featureVector.emplace_back(static_cast<double>(trackFraction));
+        this->PopulateFeatureVector(caloHitListU, featureVector);
+        this->PopulateFeatureVector(caloHitListV, featureVector);
+        this->PopulateFeatureVector(caloHitListW, featureVector);
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, LArMvaHelper::ProduceTrainingExample(m_trainingFileName,
+            trackFraction > 0.5f, featureVector));
+    }
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DlPfoCharacterisationAlgorithm::ChangeCharacterisation(const ParticleFlowObject *const pPfo) const
+{
+    PandoraContentApi::ParticleFlowObject::Metadata pfoMetadata;
+    pfoMetadata.m_particleId = pPfo->GetParticleId() == MU_MINUS ? E_MINUS : MU_MINUS;
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::ParticleFlowObject::AlterMetadata(*this, pPfo, pfoMetadata));
+
+    ClusterList clusterList;
+    LArPfoHelper::GetTwoDClusterList(pPfo, clusterList);
+    for (const Cluster *const pCluster : clusterList)
+    {
+        if (pCluster->GetParticleId() == pfoMetadata.m_particleId.Get())
+            continue;
+
+        PandoraContentApi::Cluster::Metadata clusterMetadata;
+        clusterMetadata.m_particleId = pfoMetadata.m_particleId.Get();
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::Cluster::AlterMetadata(*this, pCluster, clusterMetadata));
+    }
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void DlPfoCharacterisationAlgorithm::PopulateFeatureVector(const CaloHitList &caloHitList, LArMvaHelper::MvaFeatureVector &featureVector) const
+{
+    featureVector.emplace_back(static_cast<double>(caloHitList.size()));
+    for (const CaloHit *pCaloHit : caloHitList)
+    {
+        featureVector.emplace_back(static_cast<double>(pCaloHit->GetPositionVector().GetX()));
+        featureVector.emplace_back(static_cast<double>(pCaloHit->GetPositionVector().GetZ()));
+        featureVector.emplace_back(static_cast<double>(pCaloHit->GetInputEnergy()));
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 StatusCode DlPfoCharacterisationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
-    return PfoCharacterisationBaseAlgorithm::ReadSettings(xmlHandle);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "TrackPfoListName", m_trackPfoListName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "ShowerPfoListName", m_showerPfoListName));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "Training", m_training));
+
+    if (m_training)
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "TrainingFileName", m_trainingFileName));
+    }
+    else
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "ImageHeight", m_imageHeight));
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "ImageWidth", m_imageWidth));
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "ModelFileName", m_modelFileName));
+        m_modelFileName = LArFileHelper::FindFileInPath(m_modelFileName, "FW_SEARCH_PATH");
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, LArDLHelper::LoadModel(m_modelFileName, m_model));
+    }
+
+    return STATUS_CODE_SUCCESS;
 }
 
 } // namespace lar_dl_content
--- larpandoradlcontent/LArTrackShowerId/DlPfoCharacterisationAlgorithm.h
+++ larpandoradlcontent/LArTrackShowerId/DlPfoCharacterisationAlgorithm.h
@@ -8,7 +8,12 @@
 #ifndef LAR_DL_PFO_CHARACTERISATION_ALGORITHM_H
 #define LAR_DL_PFO_CHARACTERISATION_ALGORITHM_H 1
 
-#include "larpandoracontent/LArTrackShowerId/PfoCharacterisationBaseAlgorithm.h"
+#include "Pandora/Algorithm.h"
+
+#include "larpandoracontent/LArHelpers/LArMvaHelper.h"
+#include "larpandoracontent/LArHelpers/LArPfoHelper.h"
+
+#include "larpandoradlcontent/LArHelpers/LArDLHelper.h"
 
 namespace lar_dl_content
 {
@@ -16,7 +21,7 @@ namespace lar_dl_content
 /**
  *  @brief  DlPfoCharacterisationAlgorithm class
  */
-class DlPfoCharacterisationAlgorithm : public lar_content::PfoCharacterisationBaseAlgorithm
+class DlPfoCharacterisationAlgorithm : public pandora::Algorithm
 {
 public:
     /**
@@ -25,9 +30,64 @@ public:
     DlPfoCharacterisationAlgorithm();
 
 private:
-    bool IsClearTrack(const pandora::Cluster *const pCluster) const;
-    bool IsClearTrack(const pandora::ParticleFlowObject *const pPfo) const;
+    pandora::StatusCode Run();
+
+    /**
+     *  @brief  Run the network inference to characterise the PFOs
+     */
+    pandora::StatusCode Infer();
+
+    /**
+     *  @brief  Produce files that act as inputs to network training
+     */
+    pandora::StatusCode Train();
+
+    /**
+     *  @brief  Add calo hits to the input for the network
+     *
+     *  @param  caloHitList The calo hits to be added
+     *  @param  input The network input tensor to be populated
+     */
+    void PrepareNetworkInput(const pandora::CaloHitList &caloHitList, LArDLHelper::TorchInput &input) const;
+
+    /**
+     *  @brief  Identify the XZ range containing the hits for an event
+     *
+     *  @param  caloHitList The list of CaloHits for which the range is to be found
+     *  @param  xMin The output minimum x-coordinate
+     *  @param  xMax The output maximum x-coordinate
+     *  @param  zMin The output minimum z-coordinate
+     *  @param  zMax The output maximum z-coordinate
+     */
+    void GetHitRegion(const pandora::CaloHitList &caloHitList, double &xMin, double &xMax, double &zMin, double &zMax) const;
+
+    /**
+     *  @brief  Process a single PFO list
+     */
+    pandora::StatusCode ProcessPfoList(const std::string &pfoListName) const;
+
+    /**
+     *  @brief  Change the existing track/shower characterisation of a PFO
+     *
+     *  @param  pfo The particle flow object whose characterisation should be changed
+     */
+    pandora::StatusCode ChangeCharacterisation(const pandora::ParticleFlowObject *const pPfo) const;
+
+    /**
+     *  @brief  Adds hit information from a hit list to a feature vector
+     */
+    void PopulateFeatureVector(const pandora::CaloHitList &caloHitList, lar_content::LArMvaHelper::MvaFeatureVector &featureVector) const;
+
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    bool m_training;    ///< Whether or not to run in training mode
+    int m_imageWidth; ///< The width of event images
+    int m_imageHeight; ///< The height of event images
+    std::string m_trackPfoListName; ///< Name of input track PFO list
+    std::string m_showerPfoListName; ///< Name of input shower PFO list
+    std::string m_trainingFileName; ///< Name of the output training file
+    std::string m_modelFileName; ///< Model file name
+    LArDLHelper::TorchModel m_model; ///< Model
 };
 
 } // namespace lar_dl_content
