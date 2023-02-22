01d9581a04d25396a3988492cc459c5207732878
Andrew Chappell <A.Chappell@warwick.ac.uk>
2023-02-22 09:18:53 +0000

    Split clusters based on secondary vertices

--- larpandoracontent/LArTwoDReco/LArClusterSplitting/VertexSplittingAlgorithm.cc
+++ larpandoracontent/LArTwoDReco/LArClusterSplitting/VertexSplittingAlgorithm.cc
@@ -18,7 +18,11 @@ using namespace pandora;
 namespace lar_content
 {
 
-VertexSplittingAlgorithm::VertexSplittingAlgorithm() : m_splitDisplacementSquared(4.f * 4.f), m_vertexDisplacementSquared(1.f * 1.f)
+VertexSplittingAlgorithm::VertexSplittingAlgorithm() :
+    m_splitDisplacementSquared(4.f * 4.f),
+    m_vertexDisplacementSquared(1.f * 1.f),
+    m_useSecondaryVertices{false},
+    m_vertexListName{""}
 {
     // ATTN Some default values differ from base class
     m_minClusterLength = 1.f;
@@ -30,48 +34,65 @@ StatusCode VertexSplittingAlgorithm::FindBestSplitPosition(const TwoDSlidingFitR
 {
     // Identify event vertex
     const VertexList *pVertexList(NULL);
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pVertexList));
+    if (m_vertexListName.empty())
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pVertexList));
+    }
+    else
+    {
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_vertexListName, pVertexList));
+    }
 
     if (pVertexList->empty())
         return STATUS_CODE_NOT_INITIALIZED;
 
-    if (pVertexList->size() != 1)
+    if (!m_useSecondaryVertices && pVertexList->size() != 1)
         return STATUS_CODE_OUT_OF_RANGE;
 
     const Cluster *const pCluster(slidingFitResult.GetCluster());
     const HitType hitType(LArClusterHelper::GetClusterHitType(pCluster));
-
-    const Vertex *const pSelectedVertex(*(pVertexList->begin()));
-
-    if (VERTEX_3D != pSelectedVertex->GetVertexType())
-        return STATUS_CODE_INVALID_PARAMETER;
-
-    const CartesianVector theVertex2D(LArGeometryHelper::ProjectPosition(this->GetPandora(), pSelectedVertex->GetPosition(), hitType));
-
-    const CartesianVector innerVertex2D(slidingFitResult.GetGlobalMinLayerPosition());
-    const CartesianVector outerVertex2D(slidingFitResult.GetGlobalMaxLayerPosition());
-
-    if ((outerVertex2D - innerVertex2D).GetMagnitudeSquared() < 4.f * m_vertexDisplacementSquared)
-        return STATUS_CODE_NOT_FOUND;
-
+    float bestVertexDisplacementSquared{std::numeric_limits<float>::max()};
+    CartesianVector bestSplitPosition(0, 0, 0);
     bool foundSplit(false);
-    const StatusCode statusCode(slidingFitResult.GetGlobalFitProjection(theVertex2D, splitPosition));
 
-    if (STATUS_CODE_SUCCESS != statusCode)
-        return statusCode;
-
-    const float splitDisplacementSquared((splitPosition - theVertex2D).GetMagnitudeSquared());
-    const float vertexDisplacementSquared(
-        std::min((splitPosition - innerVertex2D).GetMagnitudeSquared(), (splitPosition - outerVertex2D).GetMagnitudeSquared()));
-
-    if ((splitDisplacementSquared < m_splitDisplacementSquared) && (vertexDisplacementSquared > m_vertexDisplacementSquared) &&
-        (splitDisplacementSquared < vertexDisplacementSquared))
+    for (const Vertex *const pSelectedVertex : *pVertexList)
     {
-        foundSplit = true;
+        if (VERTEX_3D != pSelectedVertex->GetVertexType())
+            return STATUS_CODE_INVALID_PARAMETER;
+
+        const CartesianVector theVertex2D(LArGeometryHelper::ProjectPosition(this->GetPandora(), pSelectedVertex->GetPosition(), hitType));
+
+        const CartesianVector innerVertex2D(slidingFitResult.GetGlobalMinLayerPosition());
+        const CartesianVector outerVertex2D(slidingFitResult.GetGlobalMaxLayerPosition());
+
+        if ((outerVertex2D - innerVertex2D).GetMagnitudeSquared() < 4.f * m_vertexDisplacementSquared)
+            return STATUS_CODE_NOT_FOUND;
+
+        const StatusCode statusCode(slidingFitResult.GetGlobalFitProjection(theVertex2D, splitPosition));
+
+        if (STATUS_CODE_SUCCESS != statusCode)
+            return statusCode;
+
+        const float splitDisplacementSquared((splitPosition - theVertex2D).GetMagnitudeSquared());
+        const float vertexDisplacementSquared(
+            std::min((splitPosition - innerVertex2D).GetMagnitudeSquared(), (splitPosition - outerVertex2D).GetMagnitudeSquared()));
+
+        if ((splitDisplacementSquared < m_splitDisplacementSquared) && (vertexDisplacementSquared > m_vertexDisplacementSquared) &&
+            (splitDisplacementSquared < vertexDisplacementSquared))
+        {
+            foundSplit = true;
+            if (vertexDisplacementSquared < bestVertexDisplacementSquared)
+            {
+                bestVertexDisplacementSquared = vertexDisplacementSquared;
+                bestSplitPosition.SetValues(splitPosition.GetX(), splitPosition.GetY(), splitPosition.GetZ());
+            }
+        }
     }
 
     if (!foundSplit)
         return STATUS_CODE_NOT_FOUND;
+
+    splitPosition.SetValues(bestSplitPosition.GetX(), bestSplitPosition.GetY(), bestSplitPosition.GetZ());
 
     return STATUS_CODE_SUCCESS;
 }
@@ -89,6 +110,9 @@ StatusCode VertexSplittingAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
     PANDORA_RETURN_RESULT_IF_AND_IF(
         STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "VertexDisplacement", vertexDisplacement));
     m_vertexDisplacementSquared = vertexDisplacement * vertexDisplacement;
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "UseSecondaryVertices", m_useSecondaryVertices));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "VertexListName", m_vertexListName));
 
     return TwoDSlidingFitSplittingAlgorithm::ReadSettings(xmlHandle);
 }
