7840e674bceffa19dc7193e32224fe3661fbc21f
Andrew Chappell <A.Chappell@warwick.ac.uk>
2023-07-11 18:14:01 +0100

    Implement basic proximity and PCA based cluster merging

--- larpandoracontent/LArWorkshop/MyClusterMergingAlgorithm.cc
+++ larpandoracontent/LArWorkshop/MyClusterMergingAlgorithm.cc
@@ -8,6 +8,7 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 #include "larpandoracontent/LArHelpers/LArClusterHelper.h"
+#include "larpandoracontent/LArHelpers/LArPcaHelper.h"
 
 #include "larpandoracontent/LArWorkshop/MyClusterMergingAlgorithm.h"
 
@@ -31,6 +32,7 @@ StatusCode MyClusterMergingAlgorithm::Run()
     if (pClusterList->empty())
         return STATUS_CODE_SUCCESS;
 
+    // Loop over all combinations of cluster pairs and see if they are associated
     for (auto iter1 = pClusterList->begin(); iter1 != pClusterList->end(); ++iter1)
     {
         const Cluster *const pCluster1{*iter1};
@@ -45,6 +47,7 @@ StatusCode MyClusterMergingAlgorithm::Run()
         }
     }
 
+    // Merge associated clusters and delete the originals
     this->MergeClusters(clusterAssocMap);
 
     return STATUS_CODE_SUCCESS;
@@ -54,10 +57,29 @@ StatusCode MyClusterMergingAlgorithm::Run()
 
 bool MyClusterMergingAlgorithm::AreClustersAssociated(const Cluster *const pCluster1, const Cluster *const pCluster2)
 {
+    // Perform a simple proximity test
     if (LArClusterHelper::GetClosestDistance(pCluster1, pCluster2) > m_maxClusterSeparation)
         return false;
 
-    return true;
+    // Get the calo hits from the two clusters under consideration
+    CaloHitList caloHitList1;
+    pCluster1->GetOrderedCaloHitList().FillCaloHitList(caloHitList1);
+    CaloHitList caloHitList2;
+    pCluster2->GetOrderedCaloHitList().FillCaloHitList(caloHitList2);
+
+    // Get the PCA axes for each cluster
+    CartesianVector centroid1(0, 0, 0);
+    LArPcaHelper::EigenValues eigenValues1(0, 0, 0);
+    LArPcaHelper::EigenVectors eigenVectors1;
+    LArPcaHelper::RunPca(caloHitList1, centroid1, eigenValues1, eigenVectors1);
+
+    CartesianVector centroid2(0, 0, 0);
+    LArPcaHelper::EigenValues eigenValues2(0, 0, 0);
+    LArPcaHelper::EigenVectors eigenVectors2;
+    LArPcaHelper::RunPca(caloHitList2, centroid2, eigenValues2, eigenVectors2);
+
+    // Check the dot product to see if the trajectories are consistent
+    return std::abs(eigenVectors1.front().GetDotProduct(eigenVectors2.front())) > 0.99f;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -69,13 +91,14 @@ StatusCode MyClusterMergingAlgorithm::MergeClusters(const ClusterMergeMap &clust
     for (const auto &[pSeedCluster, associatedClusterList] : clusterMergeMap)
     {
         ClusterSet clustersToMerge;
+        // Skip clusters that have already been merged in an earlier pass
         if (usedClusters.find(pSeedCluster) != usedClusters.end())
-        {
             continue;
-        }
 
         usedClusters.insert(pSeedCluster);
+        // Identify chains of associations for more inclusive merging
         this->TraceAssociations(clusterMergeMap, pSeedCluster, usedClusters, clustersToMerge);
+        // Perform the merge by growing the seed cluster and deleting the associated cluster
         for (const Cluster *pCluster : clustersToMerge)
         {
             PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::MergeAndDeleteClusters(*this, pSeedCluster, pCluster,
@@ -91,6 +114,7 @@ StatusCode MyClusterMergingAlgorithm::MergeClusters(const ClusterMergeMap &clust
 void MyClusterMergingAlgorithm::TraceAssociations(const ClusterMergeMap &clusterMergeMap, const Cluster *const pSeedCluster,
     ClusterSet &usedClusters, ClusterSet &clustersToMerge)
 {
+    // Find all of the clusters deemed associated to our seed cluster and that have not previously been merged
     for (const Cluster *pCluster : clusterMergeMap.at(pSeedCluster))
     {
         if (usedClusters.find(pCluster) != usedClusters.end())
@@ -101,6 +125,7 @@ void MyClusterMergingAlgorithm::TraceAssociations(const ClusterMergeMap &cluster
         {
             clustersToMerge.insert(pCluster);
             usedClusters.insert(pCluster);
+            // Use the current cluster as the new seed cluster to continue expanding the merge
             this->TraceAssociations(clusterMergeMap, pCluster, usedClusters, clustersToMerge);
         }
     }
