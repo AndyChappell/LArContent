d7508a3f83f7c4f8ffa02b76491975020429ed1e
Andrew Chappell <A.Chappell@warwick.ac.uk>
2024-12-03 09:27:47 +0000

    Check for continuity of connecting paths and merge paths

--- larpandoracontent/LArTwoDReco/LArClusterCreation/VertexClusterCreationAlgorithm.h
+++ larpandoracontent/LArTwoDReco/LArClusterCreation/VertexClusterCreationAlgorithm.h
@@ -29,10 +29,14 @@ public:
     VertexClusterCreationAlgorithm();
 
 private:
+    typedef std::map<const pandora::CaloHit *, bool> CaloHitUseMap;
+
     pandora::StatusCode Run();
     void GetListOfCleanClusters(const pandora::ClusterList *const pClusterList, pandora::ClusterVector &clusterVector) const;
     void IdentifyTrackStubs(const pandora::CaloHitList &caloHitList, const pandora::Vertex &vertex) const;
-    void IdentifyConnectingPaths(const pandora::CaloHitList &caloHitList, const pandora::Vertex &vertex) const;
+    void IdentifyConnectingPaths(const pandora::CaloHitList &caloHitList, const pandora::Vertex &vertex, CaloHitUseMap &usedHits) const;
+    void CheckConnectingPath(const pandora::CaloHitList &caloHitList, const pandora::Cluster *const pCluster, const pandora::CartesianVector &clusterPos,
+        const pandora::CartesianVector &clusterDir, const pandora::CartesianVector &vertexPos, CaloHitUseMap &usedHits) const;
     void ClusterHits(const pandora::CaloHitVector &caloHitVector) const;
     float GetClosestApproach(const pandora::CaloHitVector &caloHitVector, const pandora::CartesianVector &vertex) const;
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
@@ -41,6 +45,7 @@ private:
     std::string m_vertexListName;   ///< The name of the vertex list
 
     float m_hitRadii; ///< The search radius to collect hits around the vertex
+    float m_maxGap; ///< The maximum gap to allow in constructing paths back to vertices
 };
 
 } // namespace lar_content
