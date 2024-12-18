69c78a718684f73f9c106e90ec359e5c1989a7b9
Andrew Chappell <A.Chappell@warwick.ac.uk>
2024-12-18 10:54:09 +0000

    Build matching clusters in remaining two views

--- larpandoracontent/LArCheating/CheatingMatchedClusteringAlgorithm.cc
+++ larpandoracontent/LArCheating/CheatingMatchedClusteringAlgorithm.cc
@@ -13,6 +13,7 @@
 #include "larpandoracontent/LArHelpers/LArClusterHelper.h"
 #include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
 #include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
+#include "larpandoracontent/LArHelpers/LArPcaHelper.h"
 
 using namespace pandora;
 
@@ -70,10 +71,13 @@ StatusCode CheatingMatchedClusteringAlgorithm::Run()
     // Loop over the clusters from the seed view and find the corresponding clusters in the other view
     const ClusterList *pClusterList{nullptr};
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_seedClusterListName, pClusterList));
+    if (!pClusterList || pClusterList->empty())
+        return STATUS_CODE_SUCCESS;
     std::set<const CaloHit *> usedHits;
+    std::map<const Cluster*, std::map<HitType, CaloHitList>> clusterMatches;
+    const HitType seedView{LArClusterHelper::GetClusterHitType(pClusterList->front())};
     for (const Cluster *const pCluster : *pClusterList)
     {
-        const HitType seedView{LArClusterHelper::GetClusterHitType(pCluster)};
         // Collect the hits by MC and sort each MC's hits by X position (this simplifies the matching process)
         CaloHitList seedClusterHits;
         pCluster->GetOrderedCaloHitList().FillCaloHitList(seedClusterHits);
@@ -86,6 +90,7 @@ StatusCode CheatingMatchedClusteringAlgorithm::Run()
             {
                 const MCParticle *const pMC{MCParticleHelper::GetMainMCParticle(pCaloHit)};
                 seedMCHitMap[pMC].emplace_back(pCaloHit);
+                clusterMatches[pCluster][seedView].emplace_back(pCaloHit);
             }
             catch (StatusCodeException &)
             {
@@ -188,11 +193,59 @@ StatusCode CheatingMatchedClusteringAlgorithm::Run()
                         usedHits.insert(pCaloHit);
                         usedHits.insert(match.first);
                         usedHits.insert(match.second);
+                        clusterMatches[pCluster][views[0]].emplace_back(match.first);
+                        clusterMatches[pCluster][views[1]].emplace_back(match.second);
                     }
                 }
             }
             else if (views.size() == 1)
             {
+                // If we have two views populated, find the overlapping hits
+                for (const CaloHit *const pCaloHit : hits)
+                {
+                    const float x0{pCaloHit->GetPositionVector().GetX()};
+                    const float lo0{x0 - 0.5f * pCaloHit->GetCellSize1()};
+                    const float hi0{x0 + 0.5f * pCaloHit->GetCellSize1()};
+                    CaloHitVector view1Hits;
+                    for (const CaloHit *const pCaloHit1 : viewMCHitMap[views.at(0)][pMC])
+                    {
+                        const float x1{pCaloHit1->GetPositionVector().GetX()};
+                        const float lo1{x1 - 0.5f * pCaloHit1->GetCellSize1()};
+                        const float hi1{x1 + 0.5f * pCaloHit1->GetCellSize1()};
+                        if ((x0 >= lo1 && x0 <= hi1) || (x1 >= lo0 && x1 <= hi0))
+                            view1Hits.emplace_back(pCaloHit1);
+                    }
+                    // Identify the best match, if it exists, within those candidates
+                    float bestChi2{std::numeric_limits<float>::max()};
+                    const CaloHit *match{nullptr};
+                    for (const CaloHit *const pCaloHit1 : view1Hits)
+                    {
+                        if (usedHits.find(pCaloHit1) != usedHits.end())
+                            continue;
+                        float chi2{0.f};
+                        const CartesianVector &pos0(pCaloHit->GetPositionVector()), &pos1(pCaloHit1->GetPositionVector());
+                        CartesianVector pos3D(0, 0, 0);
+                        LArGeometryHelper::MergeTwoPositions3D(this->GetPandora(), seedView, views[0], pos0, pos1, pos3D, chi2);
+                        float err{LArGeometryHelper::GetSigmaUVW(this->GetPandora())};
+                        chi2 *= err * err;
+                        // Adjust the chi2 to factor in drift error
+                        err += 0.5f * pCaloHit->GetCellSize1() + 0.5f * pCaloHit1->GetCellSize1();
+                        chi2 /= err * err;
+                        if (chi2 < 1 && chi2 < bestChi2)
+                        {
+                            bestChi2 = chi2;
+                            match = pCaloHit1;
+                        }
+                    }
+                    // Record the best match
+                    if (bestChi2 < std::numeric_limits<float>::max())
+                    {
+                        matches[views[0]].emplace_back(match);
+                        usedHits.insert(pCaloHit);
+                        usedHits.insert(match);
+                        clusterMatches[pCluster][views[0]].emplace_back(match);
+                    }
+                }
             }
         }
         if (m_visualize)
@@ -205,10 +258,11 @@ StatusCode CheatingMatchedClusteringAlgorithm::Run()
                 views.emplace_back(view);
             }
             const ClusterList seedClusterList({pCluster});
-            PANDORA_MONITORING_API(VisualizeClusters(this->GetPandora(), &seedClusterList, "seed", BLACK));
+            /*PANDORA_MONITORING_API(VisualizeClusters(this->GetPandora(), &seedClusterList, "seed", BLACK));
             PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &matches[views[0]], std::to_string(views[0]), RED));
             PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &matches[views[1]], std::to_string(views[1]), BLUE));
-            PANDORA_MONITORING_API(ViewEvent(this->GetPandora()));
+            PANDORA_MONITORING_API(ViewEvent(this->GetPandora()));*/
+            // Need to actually store/make these clusters
         }
     }
     // Check for unmatched hits
@@ -221,11 +275,127 @@ StatusCode CheatingMatchedClusteringAlgorithm::Run()
                 unusedViewHitListMap[view].emplace_back(pCaloHit);
         }
     }
-    std::cout << "Unused hits: " << unusedViewHitListMap[TPC_VIEW_U].size() << " " << unusedViewHitListMap[TPC_VIEW_V].size() <<
-        " " << unusedViewHitListMap[TPC_VIEW_W].size() << std::endl;
+
+    for (const Cluster *const pCluster : *pClusterList)
+    {
+        std::map<HitType, CaloHitList> matches;
+        CaloHitList seedClusterHits;
+        pCluster->GetOrderedCaloHitList().FillCaloHitList(seedClusterHits);
+
+        // Organise the existing matched hits for this cluster into their corresponding views and MC particles
+        viewMCHitMap.clear();
+        for (const auto &[view, caloHitList] : clusterMatches[pCluster])
+        {
+            for (const CaloHit *pCaloHit : caloHitList)
+            {
+                try
+                {
+                    const MCParticle *const pMC{MCParticleHelper::GetMainMCParticle(pCaloHit)};
+                    viewMCHitMap[view][pMC].emplace_back(pCaloHit);
+                }
+                catch (StatusCodeException &)
+                {
+                }
+            }
+            for (auto &[pMC, hits] : viewMCHitMap[view])
+                std::sort(hits.begin(), hits.end(), LArClusterHelper::SortHitsByPositionInX);
+        }
+
+        for (const auto &[view, unusedCaloHitList] : unusedViewHitListMap)
+        {
+            for (const CaloHit *pCaloHit : unusedCaloHitList)
+            {
+                if (usedHits.find(pCaloHit) != usedHits.end())
+                    continue;
+                try
+                {
+                    const MCParticle *const pMC{MCParticleHelper::GetMainMCParticle(pCaloHit)};
+                    const CartesianVector &thisPos{pCaloHit->GetPositionVector()};
+                    if (viewMCHitMap[view][pMC].size() < 2)
+                        continue;
+                    LArPcaHelper::WeightedPointVector points;
+                    for (const CaloHit *const pClusterHit : viewMCHitMap[view][pMC])
+                    {
+                        const CartesianVector &otherPos{pClusterHit->GetPositionVector()};
+                        const float weight{1.f / (LArGeometryHelper::GetWirePitch(this->GetPandora(), view) + thisPos.GetDistanceSquared(otherPos))};
+                        points.emplace_back(std::make_pair(otherPos, weight));
+                    }
+                    CartesianVector centroid(0, 0, 0);
+                    LArPcaHelper::EigenValues eigenValues(0, 0, 0);
+                    LArPcaHelper::EigenVectors eigenVectors;
+                    LArPcaHelper::RunPca(points, centroid, eigenValues, eigenVectors);
+                    float minL{std::numeric_limits<float>::max()}, maxL{std::numeric_limits<float>::lowest()};
+                    float minT{std::numeric_limits<float>::max()}, maxT{std::numeric_limits<float>::lowest()};
+                    for (const CaloHit *const pClusterHit : viewMCHitMap[view][pMC])
+                    {
+                        const CartesianVector vec{pClusterHit->GetPositionVector() - centroid};
+                        const float l{eigenVectors[0].GetDotProduct(vec)};
+                        const float t{eigenVectors[1].GetDotProduct(vec)};
+                        if (l < minL)
+                            minL = l;
+                        if (l > maxL)
+                            maxL = l;
+                        if (t < minT)
+                            minT = t;
+                        if (t > maxT)
+                            maxT = t;
+                    }
+                    const CartesianVector vec{thisPos - centroid};
+                    const float l{eigenVectors[0].GetDotProduct(vec)};
+                    const float t{eigenVectors[1].GetDotProduct(vec)};
+                    if (l >= minL && l <= maxL && t >= minT && t <= maxT)
+                    {
+                        matches[view].emplace_back(pCaloHit);
+                        usedHits.insert(pCaloHit);
+                        clusterMatches[pCluster][view].emplace_back(pCaloHit);
+                    }
+                }
+                catch (StatusCodeException &)
+                {
+                }
+            }
+        }
+    }
+
+    unusedViewHitListMap.clear();
+    for (const auto &[view, pCaloHitList] : viewHitListMap)
+    {
+        for (const CaloHit *const pCaloHit : *pCaloHitList)
+        {
+            if (usedHits.find(pCaloHit) == usedHits.end())
+                unusedViewHitListMap[view].emplace_back(pCaloHit);
+        }
+    }
+
+    for (const HitType view : {TPC_VIEW_U, TPC_VIEW_V, TPC_VIEW_W})
+    {
+        if (view == seedView)
+            continue;
+        const ClusterList *pNewClusterList{nullptr};
+        std::string temporaryListName;
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::CreateTemporaryListAndSetCurrent(*this, pNewClusterList, temporaryListName));
+        for (const auto &[pCluster, viewHitMap] : clusterMatches)
+        {
+            if (viewHitMap.find(view) == viewHitMap.end())
+                continue;
+            const Cluster *pNewCluster{nullptr};
+            PandoraContentApi::Cluster::Parameters parameters;
+            parameters.m_caloHitList.insert(parameters.m_caloHitList.end(), viewHitMap.at(view).begin(), viewHitMap.at(view).end());
+            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::Cluster::Create(*this, parameters, pNewCluster));
+        }
+        std::string clusterListName{view == TPC_VIEW_U ? "ClustersU" : view == TPC_VIEW_V ? "ClustersV" : "ClustersW"};
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::SaveList<Cluster>(*this, clusterListName));
+    }
 
     if (m_visualize)
     {
+        for (const auto &[pCluster, viewHitMap] : clusterMatches)
+        {
+            for (const auto &[view, caloHitList] : viewHitMap)
+            {
+                PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &caloHitList, "cluster_" + std::to_string(view), AUTOITER));
+            }
+        }
         PANDORA_MONITORING_API(ViewEvent(this->GetPandora()));
     }
 
