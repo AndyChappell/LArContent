b0606ed1d18326f80d4098569f19e378974f6c2e
Andrew Chappell <A.Chappell@warwick.ac.uk>
2025-03-03 12:44:38 +0000

    Add container to organise hits into time slices and use as basis for Kalman-based clustering

--- larpandoracontent/LArTwoDReco/LArClusterCreation/KalmanClusterCreationAlgorithm.cc
+++ larpandoracontent/LArTwoDReco/LArClusterCreation/KalmanClusterCreationAlgorithm.cc
@@ -10,6 +10,7 @@
 
 #include "larpandoracontent/LArHelpers/LArClusterHelper.h"
 #include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
+#include "larpandoracontent/LArObjects/LArCaloHit.h"
 
 #include "larpandoracontent/LArTwoDReco/LArClusterCreation/KalmanClusterCreationAlgorithm.h"
 
@@ -25,8 +26,23 @@ KalmanClusterCreationAlgorithm::KalmanClusterCreationAlgorithm() :
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+KalmanClusterCreationAlgorithm::~KalmanClusterCreationAlgorithm()
+{
+    this->Reset();
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 StatusCode KalmanClusterCreationAlgorithm::Run()
 {
+    this->Reset();
+    std::map<HitType, int> viewHitCountMap;
+    for (const HitType view : {TPC_VIEW_U, TPC_VIEW_V, TPC_VIEW_W})
+    {
+        m_viewHitsMap[view] = OrderedCaloHitList();
+        viewHitCountMap[view] = 0;
+    }
+    // Collect hits from all views into layer-ordered hit lists
     for (std::string listName : m_caloHitListNames)
     {
         const CaloHitList *pCaloHitList{nullptr};
@@ -35,18 +51,111 @@ StatusCode KalmanClusterCreationAlgorithm::Run()
         {
             HitType view{pCaloHitList->front()->GetHitType()};
             PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->FilterCaloHits(pCaloHitList, m_viewHitsMap[view]));
+            for (OrderedCaloHitList::const_iterator iter = m_viewHitsMap[view].begin(), iterEnd = m_viewHitsMap[view].end(); iter != iterEnd; ++iter)
+                viewHitCountMap[view] += iter->second->size();
         }
+    }
+
+    float min{std::numeric_limits<float>::max()}, max{std::numeric_limits<float>::lowest()};
+    this->GetSpanX(min, max);
+    for (std::string listName : m_caloHitListNames)
+    {
+        const CaloHitList *pCaloHitList{nullptr};
+        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, listName, pCaloHitList));
+        if (!pCaloHitList->empty())
+        {
+            HitType view{pCaloHitList->front()->GetHitType()};
+            if (m_slicedCaloHits.find(view) == m_slicedCaloHits.end())
+            {
+                m_slicedCaloHits[view] = new LArSlicedCaloHitList(*pCaloHitList, min, max);
+            }
+            else
+            {
+                std::cout << "Error: KalmanClusterCreationAlgorithm - Duplciate view specified in calo hit lists" << std::endl;
+                throw StatusCodeException(STATUS_CODE_ALREADY_INITIALIZED);
+            }
+        }
+    }
+    for (HitType view : {TPC_VIEW_U, TPC_VIEW_V, TPC_VIEW_W})
+    {
+        if (m_slicedCaloHits.find(view) == m_slicedCaloHits.end())
+            m_slicedCaloHits[view] = new LArSlicedCaloHitList(CaloHitList(), min, max);
+    }
+
+    // Order the views by hit count, because we want to use the view with the most hits as our "primary" view
+    std::vector<HitType> order;
+    if (viewHitCountMap[TPC_VIEW_W] >= viewHitCountMap[TPC_VIEW_V])
+    {
+        order.emplace_back(TPC_VIEW_W);
+        order.emplace_back(TPC_VIEW_V);
+        if (viewHitCountMap[TPC_VIEW_U] > viewHitCountMap[TPC_VIEW_W])
+            order.insert(order.begin(), TPC_VIEW_U);
+        else if (viewHitCountMap[TPC_VIEW_U] <= viewHitCountMap[TPC_VIEW_V])
+            order.insert(order.end(), TPC_VIEW_U);
+        else
+            order.insert(order.begin() + 1, TPC_VIEW_U);
+    }
+    else
+    {
+        order.emplace_back(TPC_VIEW_V);
+        order.emplace_back(TPC_VIEW_W);
+        if (viewHitCountMap[TPC_VIEW_U] > viewHitCountMap[TPC_VIEW_V])
+            order.insert(order.begin(), TPC_VIEW_U);
+        else if (viewHitCountMap[TPC_VIEW_U] <= viewHitCountMap[TPC_VIEW_W])
+            order.insert(order.end(), TPC_VIEW_U);
+        else
+            order.insert(order.begin() + 1, TPC_VIEW_U);
     }
 
     for (const HitType view : {TPC_VIEW_U, TPC_VIEW_V, TPC_VIEW_W})
     {
-        size_t totalHits{0};
         for (OrderedCaloHitList::const_iterator iter = m_viewHitsMap[view].begin(), iterEnd = m_viewHitsMap[view].end(); iter != iterEnd; ++iter)
-            totalHits += iter->second->size();
-        std::cout << "View: " << view << " Filtered: " << totalHits << std::endl;
+        {
+            CaloHitVector caloHits(iter->second->begin(), iter->second->end());
+            std::sort(caloHits.begin(), caloHits.end(), LArClusterHelper::SortHitsByPosition);
+        }
+    }
+    if (viewHitCountMap[order.back()] > 0)
+    {
+        // All three views have hits
+        this->IdentifyCandidateClusters(order);
+    }
+    else if (viewHitCountMap[order[1]] > 0)
+    {
+        // Only two views have hits - suspect we won't need this special case
     }
 
     return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanClusterCreationAlgorithm::IdentifyCandidateClusters(const ViewVector &order)
+{
+    // Begin processing the "primary" view with reference to the secondary and tertiary views
+    // HERE
+    /*
+       Basically want to loop over every hit in the primary view and consider it a starting hit for a cluster.
+       Look for nearby hits to iteratively/recirusively add to this newly created cluster.
+       Stop when additional adds look unwise/ambiguous. Be sure to track which clusters (by ID) a hit belongs
+       to, so we can figure out inter-cluster comparisons to see what the best option is - essentially we want to
+       see if the chi2 or Kalman score gets notably better/worse by including it, and then basically keep it in
+       the longest cluster that looks consistent, removing it from the others.
+     */
+    const LArSlicedCaloHitList::SliceHitMap &sliceHitMap0{m_slicedCaloHits[order.at(0)]->GetSliceHitMap()};
+    const LArSlicedCaloHitList::SliceHitMap &sliceHitMap1{m_slicedCaloHits[order.at(1)]->GetSliceHitMap()};
+    const LArSlicedCaloHitList::SliceHitMap &sliceHitMap2{m_slicedCaloHits[order.at(2)]->GetSliceHitMap()};
+    for (const auto &[bin, caloHits0_0] : sliceHitMap0)
+    {
+        CaloHitVector caloHits1_0, caloHits2_0, caloHits0_m1, caloHits0_p1, caloHits1_m1, caloHits1_p1, caloHits2_m1, caloHits2_p1;
+        this->GetSlices(sliceHitMap0, bin, caloHits0_m1, caloHits0_p1);
+        this->GetSlices(sliceHitMap1, bin, caloHits1_0, caloHits1_m1, caloHits1_p1);
+        this->GetSlices(sliceHitMap2, bin, caloHits2_0, caloHits2_m1, caloHits2_p1);
+
+        std::cout << "0: " << bin << " - [" << caloHits0_0.size() << "," << caloHits0_m1.size() << "," << caloHits0_p1.size() << "]" << std::endl;
+        std::cout << "0: " << bin << " - [" << caloHits1_0.size() << "," << caloHits1_m1.size() << "," << caloHits1_p1.size() << "]" << std::endl;
+        std::cout << "0: " << bin << " - [" << caloHits2_0.size() << "," << caloHits2_m1.size() << "," << caloHits2_p1.size() << "]" << std::endl;
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -71,12 +180,145 @@ StatusCode KalmanClusterCreationAlgorithm::FilterCaloHits(const CaloHitList *con
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+void KalmanClusterCreationAlgorithm::GetSpanX(float &min, float &max) const
+{
+    min = std::numeric_limits<float>::max();
+    max = std::numeric_limits<float>::lowest();
+
+    for (const auto &[view, orderedHitList] : m_viewHitsMap)
+    {
+        for (OrderedCaloHitList::const_iterator iter = orderedHitList.begin(), iterEnd = orderedHitList.end(); iter != iterEnd; ++iter)
+        {
+            const CaloHitList &caloHitList{*iter->second};
+            for (const CaloHit *const pCaloHit : caloHitList)
+            {
+                const float xLo{pCaloHit->GetPositionVector().GetX() - 0.5f * pCaloHit->GetCellSize1()};
+                const float xHi{pCaloHit->GetPositionVector().GetX() + 0.5f * pCaloHit->GetCellSize1()};
+                if (xLo < min)
+                    min = xLo;
+                if (xHi > max)
+                    max = xHi;
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode KalmanClusterCreationAlgorithm::Reset()
+{
+    StatusCode status{Algorithm::Reset()};
+    for (auto &[bin, slicedCaloHitList] : m_slicedCaloHits)
+    {
+        delete slicedCaloHitList;
+    }
+    m_slicedCaloHits.clear();
+    m_viewHitsMap.clear();
+
+    return status;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanClusterCreationAlgorithm::GetSlices(const LArSlicedCaloHitList::SliceHitMap &sliceHitMap, const size_t bin, CaloHitVector &caloHits_m1,
+    CaloHitVector &caloHits_p1) const
+{
+    if (sliceHitMap.find(bin - 1) != sliceHitMap.end())
+    {
+        const CaloHitVector &caloHits{sliceHitMap.at(bin - 1)};
+        caloHits_m1.insert(caloHits_m1.end(), caloHits.begin(), caloHits.end());
+    }
+    if (sliceHitMap.find(bin + 1) != sliceHitMap.end())
+    {
+        const CaloHitVector &caloHits{sliceHitMap.at(bin + 1)};
+        caloHits_p1.insert(caloHits_p1.end(), caloHits.begin(), caloHits.end());
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanClusterCreationAlgorithm::GetSlices(const LArSlicedCaloHitList::SliceHitMap &sliceHitMap, const size_t bin, CaloHitVector &caloHits_0,
+    CaloHitVector &caloHits_m1, CaloHitVector &caloHits_p1) const
+{
+    if (sliceHitMap.find(bin) != sliceHitMap.end())
+    {
+        const CaloHitVector &caloHits{sliceHitMap.at(bin)};
+        caloHits_0.insert(caloHits_0.end(), caloHits.begin(), caloHits.end());
+    }
+    this->GetSlices(sliceHitMap, bin, caloHits_m1, caloHits_p1);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 StatusCode KalmanClusterCreationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "MinMipFraction", m_minMipFraction));
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "CaloHitListNames", m_caloHitListNames));
 
     return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanClusterCreationAlgorithm::CandidateCluster::AddTriplet(const CaloHit *const pHit1, const CaloHit *const pHit2, const CaloHit *const pHit3)
+{
+    const CaloHit *pHitU{nullptr}, *pHitV{nullptr}, *pHitW{nullptr};
+    if (pHit1)
+    {
+        switch (pHit1->GetHitType())
+        {
+            case TPC_VIEW_U:
+                pHitU = pHit1;
+                break;
+            case TPC_VIEW_V:
+                pHitU = pHit1;
+                break;
+            case TPC_VIEW_W:
+                pHitU = pHit1;
+                break;
+            default:
+                std::cout << "Error: KalmanClusterCreationAlgorithm::CandidateCluster::AddTriplet - Unexpetced hit type: " << pHit1->GetHitType() << std::endl;
+                throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+        }
+    }
+    if (pHit2)
+    {
+        switch (pHit2->GetHitType())
+        {
+            case TPC_VIEW_U:
+                pHitU = pHit2;
+                break;
+            case TPC_VIEW_V:
+                pHitU = pHit2;
+                break;
+            case TPC_VIEW_W:
+                pHitU = pHit2;
+                break;
+            default:
+                std::cout << "Error: KalmanClusterCreationAlgorithm::CandidateCluster::AddTriplet - Unexpetced hit type: " << pHit2->GetHitType() << std::endl;
+                throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+        }
+    }
+    if (pHit3)
+    {
+        switch (pHit3->GetHitType())
+        {
+            case TPC_VIEW_U:
+                pHitU = pHit3;
+                break;
+            case TPC_VIEW_V:
+                pHitU = pHit3;
+                break;
+            case TPC_VIEW_W:
+                pHitU = pHit3;
+                break;
+            default:
+                std::cout << "Error: KalmanClusterCreationAlgorithm::CandidateCluster::AddTriplet - Unexpetced hit type: " << pHit3->GetHitType() << std::endl;
+                throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+        }
+    }
+    m_hitTriplets.emplace_back(std::make_tuple(pHitU, pHitV, pHitW));
 }
 
 } // namespace lar_content
--- larpandoracontent/LArTwoDReco/LArClusterCreation/KalmanClusterCreationAlgorithm.h
+++ larpandoracontent/LArTwoDReco/LArClusterCreation/KalmanClusterCreationAlgorithm.h
@@ -10,6 +10,8 @@
 
 #include "Pandora/Algorithm.h"
 
+#include "larpandoracontent/LArObjects/LArSlicedCaloHitList.h"
+
 #include <unordered_map>
 
 namespace lar_content
@@ -26,8 +28,31 @@ public:
      */
     KalmanClusterCreationAlgorithm();
 
+    /**
+     *  @brief  Destructor
+     */
+    ~KalmanClusterCreationAlgorithm();
+
 private:
+    typedef std::vector<pandora::HitType> ViewVector;
     typedef std::map<pandora::HitType, pandora::OrderedCaloHitList> ViewOrderedHitsMap;
+    typedef std::map<const pandora::CaloHit *, pandora::IntVector> HitCandidateCLusterMap;
+
+    /**
+     *  @brief  CandidateCluster class
+     */
+    class CandidateCluster
+    {
+    public:
+        typedef std::tuple<const pandora::CaloHit *, const pandora::CaloHit *, const pandora::CaloHit *> HitTriplet;
+
+        CandidateCluster() = default;
+
+        void AddTriplet(const pandora::CaloHit *const pHit1, const pandora::CaloHit *const pHit2, const pandora::CaloHit *const pHit3);
+
+    private:
+        std::vector<HitTriplet> m_hitTriplets;
+    };
 
     /**
      *  @brief  HitAssociation class
@@ -66,6 +91,11 @@ private:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
+     *  @brief  Identifies potential clusters. At this stage hits can appear in more than one cluster.
+     */
+    void IdentifyCandidateClusters(const ViewVector &order);
+
+    /**
      *  @brief  Filter out hits below any MIP threshold and organise into an ordered calo hit list
      *
      *  @param  pCaloHitList input hit list
@@ -73,9 +103,48 @@ private:
      */
     pandora::StatusCode FilterCaloHits(const pandora::CaloHitList *const pCaloHitList, pandora::OrderedCaloHitList &selectedCaloHitList) const;
 
+    /**
+     *  @brief  Retrieve the event span in x across all views
+     *
+     *  @param  min the output for the minimum x coordinate
+     *  @param  max the output for the maximum x coordinate
+     */
+    void GetSpanX(float &min, float &max) const;
+
+    /**
+     *  @brief  Clean up member variables between events
+     */
+    pandora::StatusCode Reset() override;
+
+    /**
+     *  @brief  Retrieve the slices from a given view/slice map in the vicinty of a given bin. Empty or missing bins will leave the hit lists unfilled
+     *
+     *  @param  sliceHitMap The slice-to-hit map from which slices should be retrieved
+     *  @param  bin The central bin around which slices should be retrieved
+     *  @param  caloHits_m1 The output hit list into which hits from the lesser adjacent bin (if populated) will be placed
+     *  @param  caloHits_p1 The output hit list into which hits from the greater adjacent bin (if populated) will be placed
+     */
+    void GetSlices(const LArSlicedCaloHitList::SliceHitMap &sliceHitMap, const size_t bin, pandora::CaloHitVector &caloHits_m1,
+        pandora::CaloHitVector &caloHits_p1) const;
+
+    /**
+     *  @brief  Retrieve the slices from a given view/slice map in the vicinty of a given bin. Empty or missing bins will leave the hit lists unfilled
+     *
+     *  @param  sliceHitMap The slice-to-hit map from which slices should be retrieved
+     *  @param  bin The central bin around which slices should be retrieved
+     *  @param  caloHits_0 The output hit list into which hits from the target bin (if populated) will be placed
+     *  @param  caloHits_m1 The output hit list into which hits from the lesser adjacent bin (if populated) will be placed
+     *  @param  caloHits_p1 The output hit list into which hits from the greater adjacent bin (if populated) will be placed
+     */
+    void GetSlices(const LArSlicedCaloHitList::SliceHitMap &sliceHitMap, const size_t bin, pandora::CaloHitVector &caloHits_0,
+        pandora::CaloHitVector &caloHits_m1, pandora::CaloHitVector &caloHits_p1) const;
+
+    typedef std::map<pandora::HitType, LArSlicedCaloHitList *> ViewSlicedHitsMap;
+
     float m_minMipFraction; ///< Minimum fraction of a MIP to consider a hit
     pandora::StringVector m_caloHitListNames; ///< The names of the calo hit lists to cluster
     ViewOrderedHitsMap m_viewHitsMap; ///< Map from the view to the corresponding ordered calo hits
+    ViewSlicedHitsMap m_slicedCaloHits; ///< Collection of calo hits in each view organised into slices in the drift coordinate
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
