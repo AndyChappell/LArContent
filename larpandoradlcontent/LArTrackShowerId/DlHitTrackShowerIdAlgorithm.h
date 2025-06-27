70eb1209305a922e5d76b1ca8dac69e82507b84a
Andrew Chappell <A.Chappell@warwick.ac.uk>
2025-06-27 15:05:59 +0100

    Reduce track/shower algorithm ahead of new ground truth definition

--- larpandoradlcontent/LArTrackShowerId/DlHitTrackShowerIdAlgorithm.h
+++ larpandoradlcontent/LArTrackShowerId/DlHitTrackShowerIdAlgorithm.h
@@ -29,57 +29,34 @@ public:
     virtual ~DlHitTrackShowerIdAlgorithm();
 
 private:
-    typedef std::map<const pandora::CaloHit *, std::tuple<int, int, int, int>> CaloHitToPixelMap;
-    typedef std::map<int, int> PixelToTileMap;
+    enum Category
+    {
+        MIP = 1,
+        HIP = 2,
+        SHOWER = 3,
+        MICHEL = 4,
+        DIFFUSE = 5
+    };
 
     pandora::StatusCode Run();
 
     /**
-     *  @brief  Produce files that act as inputs to network training
+     *  @brief  Prepare training sample for the network
+     *
+     *  @return The StatusCode indicating success or failure in producing a training sample
      */
-    pandora::StatusCode Train();
+    pandora::StatusCode PrepareTrainingSample();
 
     /**
      *  @brief  Run network inference
+     *
+     *  @return The StatusCode indicating success or failure in running inference
      */
     pandora::StatusCode Infer();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    /**
-     *  @brief  Identify the XZ range containing the hits for an event
-     *
-     *  @param  caloHitList The list of CaloHits for which the range is to be found
-     *  @param  xMin The output minimum x-coordinate
-     *  @param  xMax The output maximum x-coordinate
-     *  @param  zMin The output minimum z-coordinate
-     *  @param  zMax The output maximum z-coordinate
-     */
-    void GetHitRegion(const pandora::CaloHitList &caloHitList, float &xMin, float &xMax, float &zMin, float &zMax);
-
-    /**
-     *  @brief  Populate a map between pixels and tiles
-     *
-     *  @param  caloHitList The list of CaloHits for which the map is to be populated
-     *  @param  xMin The minimum x-coordinate
-     *  @param  zMin The minimum z-coordinate
-     *  @param  nTilesX The number of tiles in the x direction
-     *  @param  sparseMap The output map between pixels and tiles
-     */
-    void GetSparseTileMap(const pandora::CaloHitList &caloHitList, const float xMin, const float zMin, const int nTilesX, PixelToTileMap &sparseMap);
-
-    pandora::StringVector m_caloHitListNames; ///< Name of input calo hit list
-    std::string m_modelFileNameU;             ///< Model file name for U view
-    std::string m_modelFileNameV;             ///< Model file name for V view
-    std::string m_modelFileNameW;             ///< Model file name for W view
-    LArDLHelper::TorchModel m_modelU;         ///< Model for the U view
-    LArDLHelper::TorchModel m_modelV;         ///< Model for the V view
-    LArDLHelper::TorchModel m_modelW;         ///< Model for the W view
-    int m_imageHeight;                        ///< Height of images in pixels
-    int m_imageWidth;                         ///< Width of images in pixels
-    float m_tileSize;                         ///< Size of tile in cm
-    bool m_visualize;                         ///< Whether to visualize the track shower ID scores
-    bool m_useTrainingMode;                   ///< Training mode
-    std::string m_trainingOutputFile;         ///< Output file name for training examples
+    std::string m_caloHitListName; ///< Name of input calo hit list
+    bool m_trainingMode; ///< Training mode
 };
 
 } // namespace lar_dl_content
