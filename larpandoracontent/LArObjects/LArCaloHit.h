4d6d23b958b0c2e6a918b49b4944a2c8f991f037
Andrew Chappell <A.Chappell@warwick.ac.uk>
2025-07-09 08:09:33 +0100

    Add channel and time sigma to LArCaloHit and add LArOpHit

--- larpandoracontent/LArObjects/LArCaloHit.h
+++ larpandoracontent/LArObjects/LArCaloHit.h
@@ -29,6 +29,8 @@ class LArCaloHitParameters : public object_creation::CaloHit::Parameters
 public:
     pandora::InputUInt m_larTPCVolumeId;   ///< The lar tpc volume id
     pandora::InputUInt m_daughterVolumeId; ///< The daughter volume id
+    pandora::InputFloat m_timeSigma;       ///< The uncertainty for the signal peak
+    pandora::InputUInt m_channel;          ///< The channel of the hit
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -59,6 +61,20 @@ public:
      *  @return the daughter volume id
      */
     unsigned int GetDaughterVolumeId() const;
+
+    /**
+     *  @brief  Get the uncertainty for the signal peak
+     *
+     *  @return the uncertainty for the signal peak
+     */
+    float GetTimeSigma() const;
+
+    /**
+     *  @brief  Get the channel of the hit
+     *
+     *  @return the channel of the hit
+     */
+    unsigned int GetChannel() const;
 
     /**
      *  @brief  Fill the parameters associated with this calo hit
@@ -98,6 +114,8 @@ public:
 private:
     unsigned int m_larTPCVolumeId;   ///< The lar tpc volume id
     unsigned int m_daughterVolumeId; ///< The daughter volume id
+    float m_timeSigma;               ///< The uncertainty for the signal peak
+    unsigned int m_channel;          ///< The channel of the hit
     pandora::InputFloat m_pTrack;    ///< The probability that the hit is track-like
     pandora::InputFloat m_pShower;   ///< The probability that the hit is shower-like
 };
@@ -105,17 +123,69 @@ private:
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 /**
- *  @brief  LArCaloHitFactory responsible for object creation
+ *  @brief  LAr op hit parameters
  */
-class LArCaloHitFactory : public pandora::ObjectFactory<object_creation::CaloHit::Parameters, object_creation::CaloHit::Object>
+class LArOpHitParameters : public object_creation::CaloHit::Parameters
+{
+public:
+    pandora::InputFloat m_width; ///< The width of the optical hit
+    pandora::InputUInt m_channel; ///< The channel of the hit
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+/**
+ *  @brief  LAr optical hit class
+ */
+class LArOpHit : public object_creation::CaloHit::Object
 {
 public:
     /**
      *  @brief  Constructor
      *
-     *  @param  version the LArCaloHit version
+     *  @param  parameters the lar op hit parameters
      */
-    LArCaloHitFactory(const unsigned int version = 1);
+    LArOpHit(const LArOpHitParameters &parameters);
+
+    /**
+     *  @brief  Get the lar tpc volume id
+     *
+     *  @return the lar tpc volume id
+     */
+    float GetWidth() const;
+
+    /**
+     *  @brief  Get the channel of the hit
+     *
+     *  @return the channel of the hit
+     */
+    unsigned int GetChannel() const;
+
+    /**
+     *  @brief  Fill the parameters associated with this calo hit
+     *
+     *  @param  parameters the output parameters
+     */
+    void FillParameters(LArOpHitParameters &parameters) const;
+
+private:
+    float m_width; ///< The width of the optical hit
+    unsigned int m_channel; ///< The channel of the hit
+};
+
+
+/**
+ *  @brief  LArCaloHitFactory responsible for object creation
+ */
+class LArHitFactory : public pandora::ObjectFactory<object_creation::CaloHit::Parameters, object_creation::CaloHit::Object>
+{
+public:
+    /**
+     *  @brief  Constructor
+     *
+     *  @param  version the LAr Hit version
+     */
+    LArHitFactory(const unsigned int version = 2);
 
     /**
      *  @brief  Create new parameters instance on the heap (memory-management to be controlled by user)
@@ -149,7 +219,7 @@ public:
     pandora::StatusCode Create(const Parameters &parameters, const Object *&pObject) const;
 
 private:
-    unsigned int m_version; ///< The LArCaloHit version
+    unsigned int m_version; ///< The LAr hit version
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -242,93 +312,236 @@ inline void LArCaloHit::SetShowerProbability(const float probability)
 //------------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline LArCaloHitFactory::LArCaloHitFactory(const unsigned int version) :
+inline LArOpHit::LArOpHit(const LArOpHitParameters &parameters) :
+    object_creation::CaloHit::Object(parameters),
+    m_width(parameters.m_width.Get()),
+    m_channel(parameters.m_channel.Get())
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline float LArOpHit::GetWidth() const
+{
+    return m_width;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline unsigned int LArOpHit::GetChannel() const
+{
+    return m_channel;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void LArOpHit::FillParameters(LArOpHitParameters &parameters) const
+{
+    parameters.m_positionVector = this->GetPositionVector();
+    parameters.m_expectedDirection = this->GetExpectedDirection();
+    parameters.m_cellNormalVector = this->GetCellNormalVector();
+    parameters.m_cellGeometry = this->GetCellGeometry();
+    parameters.m_cellSize0 = this->GetCellSize0();
+    parameters.m_cellSize1 = this->GetCellSize1();
+    parameters.m_cellThickness = this->GetCellThickness();
+    parameters.m_nCellRadiationLengths = this->GetNCellRadiationLengths();
+    parameters.m_nCellInteractionLengths = this->GetNCellInteractionLengths();
+    parameters.m_time = this->GetTime();
+    parameters.m_inputEnergy = this->GetInputEnergy();
+    parameters.m_mipEquivalentEnergy = this->GetMipEquivalentEnergy();
+    parameters.m_electromagneticEnergy = this->GetElectromagneticEnergy();
+    parameters.m_hadronicEnergy = this->GetHadronicEnergy();
+    parameters.m_isDigital = this->IsDigital();
+    parameters.m_hitType = this->GetHitType();
+    parameters.m_hitRegion = this->GetHitRegion();
+    parameters.m_layer = this->GetLayer();
+    parameters.m_isInOuterSamplingLayer = this->IsInOuterSamplingLayer();
+    // ATTN Set the parent address to the original owner of the calo hit
+    parameters.m_pParentAddress = static_cast<const void *>(this);
+    parameters.m_width = this->GetWidth();
+    parameters.m_channel = this->GetChannel();
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline LArHitFactory::LArHitFactory(const unsigned int version) :
     m_version(version)
 {
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline LArCaloHitFactory::Parameters *LArCaloHitFactory::NewParameters() const
+inline LArHitFactory::Parameters *LArHitFactory::NewParameters() const
 {
-    return (new LArCaloHitParameters);
+    throw pandora::StatusCodeException(pandora::STATUS_CODE_NOT_ALLOWED);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline pandora::StatusCode LArCaloHitFactory::Create(const Parameters &parameters, const Object *&pObject) const
+inline pandora::StatusCode LArHitFactory::Create(const Parameters &parameters, const Object *&pObject) const
 {
-    const LArCaloHitParameters &larCaloHitParameters(dynamic_cast<const LArCaloHitParameters &>(parameters));
-    pObject = new LArCaloHit(larCaloHitParameters);
+    switch (parameters.m_hitType.Get())
+    {
+        case pandora::OPTICAL_SIPM:
+        case pandora::OPTICAL_TRAP:
+        case pandora::OPTICAL_TPC:
+        {
+            const LArOpHitParameters &larOpHitParameters(dynamic_cast<const LArOpHitParameters &>(parameters));
+            pObject = new LArOpHit(larOpHitParameters);
 
-    return pandora::STATUS_CODE_SUCCESS;
+            return pandora::STATUS_CODE_SUCCESS;
+        }
+        default:
+        {
+            const LArCaloHitParameters &larCaloHitParameters(dynamic_cast<const LArCaloHitParameters &>(parameters));
+            pObject = new LArCaloHit(larCaloHitParameters);
+
+            return pandora::STATUS_CODE_SUCCESS;
+        }
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline pandora::StatusCode LArCaloHitFactory::Read(Parameters &parameters, pandora::FileReader &fileReader) const
+inline pandora::StatusCode LArHitFactory::Read(Parameters &parameters, pandora::FileReader &fileReader) const
 {
     // ATTN: To receive this call-back must have already set file reader mc particle factory to this factory
-    unsigned int larTPCVolumeId(std::numeric_limits<unsigned int>::max());
-    unsigned int daughterVolumeId(0);
-
-    if (pandora::BINARY == fileReader.GetFileType())
+    switch (parameters.m_hitType.Get())
     {
-        pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader &>(fileReader));
-        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(larTPCVolumeId));
-        if (m_version > 1)
-            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(daughterVolumeId));
-    }
-    else if (pandora::XML == fileReader.GetFileType())
-    {
-        pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader &>(fileReader));
-        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("LArTPCVolumeId", larTPCVolumeId));
-        if (m_version > 1)
-            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("DaughterVolumeId", daughterVolumeId));
-    }
-    else
-    {
-        return pandora::STATUS_CODE_INVALID_PARAMETER;
-    }
+        case pandora::OPTICAL_SIPM:
+        case pandora::OPTICAL_TRAP:
+        case pandora::OPTICAL_TPC:
+        {
+            float width(0.f);
+            unsigned int channel(0);
 
-    LArCaloHitParameters &larCaloHitParameters(dynamic_cast<LArCaloHitParameters &>(parameters));
-    larCaloHitParameters.m_larTPCVolumeId = larTPCVolumeId;
-    larCaloHitParameters.m_daughterVolumeId = daughterVolumeId;
+            if (pandora::BINARY == fileReader.GetFileType())
+            {
+                pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader &>(fileReader));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(width));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(channel));
+            }
+            else if (pandora::XML == fileReader.GetFileType())
+            {
+                pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader &>(fileReader));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("OpticalWidth", width));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("OpticalChannel", channel));
+            }
+            else
+            {
+                return pandora::STATUS_CODE_INVALID_PARAMETER;
+            }
 
-    return pandora::STATUS_CODE_SUCCESS;
+            LArOpHitParameters &larOpHitParameters(dynamic_cast<LArOpHitParameters &>(parameters));
+            larOpHitParameters.m_width = width;
+            larOpHitParameters.m_channel = channel;
+
+            return pandora::STATUS_CODE_SUCCESS;
+        }
+        default:
+        {
+            unsigned int larTPCVolumeId(std::numeric_limits<unsigned int>::max());
+            unsigned int daughterVolumeId(0);
+
+            if (pandora::BINARY == fileReader.GetFileType())
+            {
+                pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader &>(fileReader));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(larTPCVolumeId));
+                if (m_version > 1)
+                    PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(daughterVolumeId));
+            }
+            else if (pandora::XML == fileReader.GetFileType())
+            {
+                pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader &>(fileReader));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("LArTPCVolumeId", larTPCVolumeId));
+                if (m_version > 1)
+                    PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("DaughterVolumeId", daughterVolumeId));
+            }
+            else
+            {
+                return pandora::STATUS_CODE_INVALID_PARAMETER;
+            }
+
+            LArCaloHitParameters &larCaloHitParameters(dynamic_cast<LArCaloHitParameters &>(parameters));
+            larCaloHitParameters.m_larTPCVolumeId = larTPCVolumeId;
+            larCaloHitParameters.m_daughterVolumeId = daughterVolumeId;
+
+            return pandora::STATUS_CODE_SUCCESS;
+        }
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline pandora::StatusCode LArCaloHitFactory::Write(const Object *const pObject, pandora::FileWriter &fileWriter) const
+inline pandora::StatusCode LArHitFactory::Write(const Object *const pObject, pandora::FileWriter &fileWriter) const
 {
     // ATTN: To receive this call-back must have already set file writer mc particle factory to this factory
-    const LArCaloHit *const pLArCaloHit(dynamic_cast<const LArCaloHit *>(pObject));
-
-    if (!pLArCaloHit)
+    const pandora::CaloHit *const pCaloHit(dynamic_cast<const pandora::CaloHit *>(pObject));
+    if (!pCaloHit)
         return pandora::STATUS_CODE_INVALID_PARAMETER;
 
-    if (pandora::BINARY == fileWriter.GetFileType())
+    switch (pCaloHit->GetHitType())
     {
-        pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter &>(fileWriter));
-        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArCaloHit->GetLArTPCVolumeId()));
-        if (m_version > 1)
-            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArCaloHit->GetDaughterVolumeId()));
-    }
-    else if (pandora::XML == fileWriter.GetFileType())
-    {
-        pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter &>(fileWriter));
-        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("LArTPCVolumeId", pLArCaloHit->GetLArTPCVolumeId()));
-        if (m_version > 1)
-            PANDORA_RETURN_RESULT_IF(
-                pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("DaughterVolumeId", pLArCaloHit->GetDaughterVolumeId()));
-    }
-    else
-    {
-        return pandora::STATUS_CODE_INVALID_PARAMETER;
-    }
+        case pandora::OPTICAL_SIPM:
+        case pandora::OPTICAL_TRAP:
+        case pandora::OPTICAL_TPC:
+        {
+            const LArOpHit *const pLArOpHit(dynamic_cast<const LArOpHit *>(pObject));
 
-    return pandora::STATUS_CODE_SUCCESS;
+            if (!pLArOpHit)
+                return pandora::STATUS_CODE_INVALID_PARAMETER;
+
+            if (pandora::BINARY == fileWriter.GetFileType())
+            {
+                pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter &>(fileWriter));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArOpHit->GetWidth()));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArOpHit->GetChannel()));
+            }
+            else if (pandora::XML == fileWriter.GetFileType())
+            {
+                pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter &>(fileWriter));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("OpticalWidth", pLArOpHit->GetWidth()));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("OpticalChannel", pLArOpHit->GetChannel()));
+            }
+            else
+            {
+                return pandora::STATUS_CODE_INVALID_PARAMETER;
+            }
+
+            return pandora::STATUS_CODE_SUCCESS;
+
+        }
+        default:
+        {
+            const LArCaloHit *const pLArCaloHit(dynamic_cast<const LArCaloHit *>(pObject));
+
+            if (!pLArCaloHit)
+                return pandora::STATUS_CODE_INVALID_PARAMETER;
+
+            if (pandora::BINARY == fileWriter.GetFileType())
+            {
+                pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter &>(fileWriter));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArCaloHit->GetLArTPCVolumeId()));
+                if (m_version > 1)
+                    PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLArCaloHit->GetDaughterVolumeId()));
+            }
+            else if (pandora::XML == fileWriter.GetFileType())
+            {
+                pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter &>(fileWriter));
+                PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("LArTPCVolumeId", pLArCaloHit->GetLArTPCVolumeId()));
+                if (m_version > 1)
+                    PANDORA_RETURN_RESULT_IF(
+                        pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("DaughterVolumeId", pLArCaloHit->GetDaughterVolumeId()));
+            }
+            else
+            {
+                return pandora::STATUS_CODE_INVALID_PARAMETER;
+            }
+
+            return pandora::STATUS_CODE_SUCCESS;
+        }
+    }
 }
 
 } // namespace lar_content
